e706f2836b4527366f7ded8cb74b6974b8bfe24d
flavioroth <flavio.roth@gmail.com>
2017-05-18 08:29:11 +0200

    Refactor some registers into specialized classes.

---
+++ lib/exec_mode.hpp
@@ -0,0 +1,13 @@
+//
+// Created by fla on 17-5-17.
+//
+
+#ifndef MICROMACHINE_EXEC_MODE_HPP
+#define MICROMACHINE_EXEC_MODE_HPP
+
+enum class exec_mode {
+	thread,
+	handler
+};
+
+#endif //MICROMACHINE_EXEC_MODE_HPP
--- lib/register.hpp
+++ lib/register.hpp
@@ -6,18 +6,14 @@
 #define MICROMACHINE_REGISTER_HPP
 
 #include "types.hpp"
+#include "exec_mode.hpp"
 
 
-
-class reg {
+class ireg {
 
 public:
 
-	reg() : _word(0) {
-
-	}
-
-	reg& operator = (word word) {
+	ireg& operator = (word word) {
 		set(word);
 		return *this;
 	}
@@ -33,16 +29,140 @@ public:
 
 private:
 
-	void set(word word)  {
+	virtual void set(word word) = 0;
+	virtual const word get() const = 0;
+
+};
+
+class standard_reg : public ireg {
+public:
+
+	using ireg::operator=;
+	standard_reg() : _word(0) {}
+
+private:
+
+	void set(word word) override {
 		_word = word;
 	}
 
-	const word get() const  {
+	const word get() const override {
 		return _word;
 	}
 
-
+protected:
 	word _word;
 };
+
+
+class control_reg : public standard_reg {
+public:
+
+	using standard_reg::standard_reg;
+	using standard_reg::operator=;
+
+	bool n_priv() const {
+		return _word.bit(0);
+	}
+
+	bool sp_sel() const {
+		return _word.bit(1);
+	}
+
+	void set_sp_sel(bool sel) {
+		_word.write_bit(1, sel);
+	}
+
+};
+
+class sp_reg : public ireg {
+public:
+
+	sp_reg(const exec_mode& exec_mode, const control_reg& control_reg)
+		: _exec_mode(exec_mode)
+		, _ctl_reg(control_reg) {
+
+	}
+
+	using ireg::operator=;
+
+private:
+
+	static const uint32_t MASK = binops::make_mask<2, 30>();
+
+	virtual void set(word word)  {
+		// these two bits should always be zero, or UNPREDICTABLE
+		if(word & MASK) {
+			// unpredicatable
+		}
+		banked() = word;
+	}
+
+	virtual const word get() const  {
+		return banked() & MASK;
+	}
+
+
+	word& banked() {
+		switch (_exec_mode) {
+			case exec_mode::thread:
+				return _sps[_ctl_reg.sp_sel()];
+			case exec_mode::handler:
+				return _sps[0];
+			default: precond_fail("invalid execution mode")
+		}
+	}
+
+	const word& banked() const {
+		switch (_exec_mode) {
+			case exec_mode::thread:
+				return _sps[_ctl_reg.sp_sel()];
+			case exec_mode::handler:
+				return _sps[0];
+			default: precond_fail("invalid execution mode")
+		}
+	}
+
+	// stack pointers
+	// 0: SP Main
+	// 1: SP Process
+	word _sps[2];
+
+
+	const exec_mode& _exec_mode;
+	const control_reg& _ctl_reg;
+};
+
+
+class pc_reg : public standard_reg {
+public:
+
+	using standard_reg::standard_reg;
+	using standard_reg::operator=;
+
+	bool dirty_status;
+
+private:
+
+	static const uint32_t MASK = binops::make_mask<1, 31>();
+
+	virtual void set(word word)  {
+
+		// if EPSR.T == 0, a UsageFault('Invalid State')
+		// is taken on the next instruction
+		if(word & MASK) {
+			// usage fault
+		}
+		_word = word;
+		dirty_status = true;
+	}
+
+	virtual const word get() const  {
+		return _word & MASK;
+	}
+};
+
+
+
 
 #endif //MICROMACHINE_REGISTER_HPP
