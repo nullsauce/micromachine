e2d50c8e301dbefe3eed9df6fcd9ad370ce93dbf
Flavio Roth <flavio.roth@gmail.com>
2018-02-26 22:23:24 +0100

    Disables high DPI scaling.

--- debugger/main.cpp
+++ debugger/main.cpp
@@ -5,7 +5,7 @@
 #include "Cpu.hpp"
 int main(int argc, char *argv[])
 {
-    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    //QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
