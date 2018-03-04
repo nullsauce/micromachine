498c489a98ac1e8745da716cf881326f665515a2
Flavio Roth <flavio.roth@gmail.com>
2018-03-04 12:24:59 +0100

    Remove old build files.

--- gen.sh
+++
@@ -1,5 +0,0 @@
-arm-none-eabi-as -march=armv6s-m test.s -o test.o && \
-arm-none-eabi-objcopy -O binary test.o test.bin && \
-arm-none-eabi-ld test.o -o test.elf && \
-xxd -i test.bin && \
-arm-none-eabi-objdump -b binary -D -marm -Mforce-thumb test.bin
--- gen2.sh
+++
@@ -1,5 +0,0 @@
-arm-none-eabi-gcc -ffreestanding -nodefaultlibs -nostdlib -nostartfiles -mthumb -march=armv6s-m testc.c -o testc.o && \
-arm-none-eabi-objcopy -O binary testc.o testc.bin && \
-arm-none-eabi-ld testc.o -o testc.elf && \
-xxd -i testc.bin && \
-arm-none-eabi-objdump -b binary -D -marm -Mforce-thumb testc.bin
