67e134d0dd9fc2ba8f884e69c1358b49f6702717
Flavio Roth <flavio.roth@gmail.com>
2018-03-04 12:27:06 +0100

    Remove non-working disassembler tests

--- test/disasm/gen.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-tmpfile=$(mktemp /tmp/gen-arm-script.XXXXXX)
-echo "$tmpfile"
-dd if=/dev/urandom of="$tmpfile" bs=1k count=1 \
- && arm-none-eabi-objdump -b binary -D -Mforce-thumb -Mreg-names-std -marm "$tmpfile" \
- | ./sanitize.sh
-rm "$tmpfile"
