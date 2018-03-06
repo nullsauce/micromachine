ce74ad89f0f4c5aa1e3ff575ab3d6c2493756893
Flavio Roth <flavio.roth@gmail.com>
2018-03-06 19:00:08 +0100

    Better handles being build from within Qt Creator.

--- env.cmake
+++ env.cmake
@@ -1,17 +1,14 @@
 
-set(QT_RUNTIME_ROOT "$ENV{HOME}/Qt/5.9.2/gcc_64")
+set(QT_RUNTIME_ROOT "$ENV{HOME}/Qt5/5.9.1/gcc_64")
 
-if(NOT IS_DIRECTORY "${Qt5_DIR}")
-    message(STATUS "Qt5_DIR not defined. Falling back to manual value.")
-    if(NOT ENV{Qt5_DIR})
-            set(Qt5_DIR "${QT_RUNTIME_ROOT}/lib/cmake/Qt5")
-    endif()
+if(DEFINED QT_QMAKE_EXECUTABLE)
+	message(STATUS "Building from Qt Creator. Using automatic Qt version.")
 else()
-    message(STATUS "Using preset value for Qt5_DIR")
-endif()
-
-if(NOT IS_DIRECTORY ${Qt5_DIR})
-    message(FATAL_ERROR "Qt5_DIR is not a directory. (Qt5_DIR=${Qt5_DIR})")
+	message(STATUS "Building outside Qt Creator. Using manual Qt install path.")
+	set(Qt5_DIR "${QT_RUNTIME_ROOT}/lib/cmake/Qt5")
+	if(NOT IS_DIRECTORY "${Qt5_DIR}")
+		message(FATAL_ERROR "Qt5_DIR not found. (Qt5_DIR=${Qt5_DIR})")
+	endif()
 endif()
 
 message(STATUS "Using Qt5 located at ${Qt5_DIR}")
