519d09647a7fb0e26916969833647088a6b43662
Flavio Roth <flavio.roth@gmail.com>
2018-03-14 00:00:36 +0100

    Include some timings in the command-line app.

--- run/main.cpp
+++ run/main.cpp
@@ -1,5 +1,6 @@
 
 
+#include <chrono>
 #include "cpu.hpp"
 
 int main(int argc, const char** argv) {
@@ -11,15 +12,24 @@ int main(int argc, const char** argv) {
 
 	cpu c;
 	c.load_elf(argv[1]);
+
+	for(int i = 0; i < 40; i++) {
 	c.reset();
 
+	auto start = std::chrono::steady_clock::now();
+	decltype(start) end;
 	for(;;) {
 		bool fault = c.step();
 		if(fault) {
+			end = std::chrono::steady_clock::now();
 			fprintf(stderr, "Too many faults. Aborting\n");
 			break;
 		}
 	}
-	c.regs().print();
+	uint64_t elapsed_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+	fprintf(stderr, "elapsed: %f seconds\n", elapsed_ms/1000.0);
+
+	}
+	//c.regs().print();
 	return EXIT_SUCCESS;
 }
