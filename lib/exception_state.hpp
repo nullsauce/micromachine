2e1c733cb6b934f217ec960ecc28fd930ab0acd3
Flavio Roth <flavio.roth@gmail.com>
2018-09-17 11:47:02 +0200

    Enables precoditions only when compile flag is set

---
+++ lib/exception_state.hpp
@@ -0,0 +1,13 @@
+/* Copyright (C) MindMaze SA - 2016 - All Rights Reserved
+
+* Unauthorized copying of this file, via any medium is strictly prohibited.
+
+* This project is proprietary and confidential. It can not be copied
+and/or distributed without the express permission of MindMaze SA.
+
+*/
+
+#ifndef MICROMACHINE_EMU_EXCEPTION_STATE_HPP_HPP
+#define MICROMACHINE_EMU_EXCEPTION_STATE_HPP_HPP
+
+#endif //MICROMACHINE_EMU_EXCEPTION_STATE_HPP_HPP
