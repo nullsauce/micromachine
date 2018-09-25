74b1fb1b967c2d1d9e724625feb20febd8820cd2
Flavio Roth <flavio.roth@gmail.com>
2018-09-25 19:34:07 +0200

    add travis-ci script

--- env.cmake
+++ env.cmake
@@ -1,5 +1,5 @@
 
-set(QT_RUNTIME_ROOT "$ENV{HOME}/Qt5/5.9.1/gcc_64")
+set(QT_RUNTIME_ROOT "$ENV{HOME}/Qt/5.9.2/gcc_64")
 
 if(DEFINED QT_QMAKE_EXECUTABLE)
 	message(STATUS "Building from Qt Creator. Using automatic Qt version.")
@@ -7,7 +7,8 @@ else()
 	message(STATUS "Building outside Qt Creator. Using manual Qt install path.")
 	set(Qt5_DIR "${QT_RUNTIME_ROOT}/lib/cmake/Qt5")
 	if(NOT IS_DIRECTORY "${Qt5_DIR}")
-		message(FATAL_ERROR "Qt5_DIR not found. (Qt5_DIR=${Qt5_DIR})")
+		message(WARNING "Qt5_DIR not found. (Qt5_DIR=${Qt5_DIR})")
+		set(Qt5_DIR "")
 	endif()
 endif()
 
