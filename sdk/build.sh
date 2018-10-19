331c309e64f36dbce0ca2f8815e7247471b1ab29
Flavio Roth <flavio.roth@gmail.com>
2018-10-19 17:15:12 +0000

    disable c++ example

--- sdk/build.sh
+++ sdk/build.sh
@@ -2,5 +2,5 @@
 
 mkdir -p build
 pushd build
-cmake -DCMAKE_TOOLCHAIN_FILE=../micromachine.toolchain ..
+cmake -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON -DCMAKE_TOOLCHAIN_FILE=../micromachine.toolchain ..
 make
