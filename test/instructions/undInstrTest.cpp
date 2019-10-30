10f495e724bd6bd710f9df9af96ad95351dccee7
Flavio Roth <flavio.roth@gmail.com>
2019-10-30 09:07:09 +0100

    Moves all intruction test into their own subfolder

---
+++ test/instructions/undInstrTest.cpp
@@ -0,0 +1,42 @@
+/*  Copyright (C) 2014  Adam Green (https://github.com/adamgreen)
+
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+*/
+#include "CpuTestHarness.hpp"
+
+
+TEST_F(CpuTestHarness, undefined_Undedfined16BitWithAllZeroesForImmedaite)
+{
+	emitInstruction16("11011110iiiiiiii", 0);
+	setExpectedExceptionTaken(CPU_STEP_HARDFAULT);
+	step();
+}
+
+TEST_F(CpuTestHarness, undefined_Undedfined16BitWithAllOnesForImmedaite)
+{
+	emitInstruction16("11011110iiiiiiii", -1);
+	setExpectedExceptionTaken(CPU_STEP_HARDFAULT);
+	step();
+}
+
+TEST_F(CpuTestHarness, undefined_Undefined32BitWithAllZeroesForImmediate)
+{
+	emitInstruction32("111101111111iiii", "1010iiiiiiiiiiii", 0, 0);
+	setExpectedExceptionTaken(CPU_STEP_HARDFAULT);
+	step();
+}
+
+TEST_F(CpuTestHarness, undefined_Undefined32BitWithAllOnesForImmediate)
+{
+	emitInstruction32("111101111111iiii", "1010iiiiiiiiiiii", -1, -1);
+	setExpectedExceptionTaken(CPU_STEP_HARDFAULT);
+	step();
+}
