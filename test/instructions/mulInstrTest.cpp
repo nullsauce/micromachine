c3cc9362376a90de48ef2976d92b3e4b2a1d9ee6
Flavio Roth <flavio.roth@mindmaze.ch>
2019-10-30 09:07:09 +0100

    Moves all intruction test into their own subfolder

---
+++ test/instructions/mulInstrTest.cpp
@@ -0,0 +1,53 @@
+/*  Copyright (C) 2014  Adam Green (https://github.com/adamgreen)
+
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+*/
+
+#include "CpuTestHarness.hpp"
+
+
+/* MUL
+   Encoding: 010000 1101 Rn:3 Rdm:3 */
+TEST_F(CpuTestHarness, mul_UseLowestRegisterForAllArgs)
+{
+	emitInstruction16("0100001101nnnddd", R0, R0);
+	setExpectedXPSRflags("nZ");
+	setExpectedRegisterValue(R0, 0U);
+	step();
+}
+
+TEST_F(CpuTestHarness, mul_UseHigestRegisterForAllArgs_OnlyGetLower32bitsOfResult)
+{
+	emitInstruction16("0100001101nnnddd", R7, R7);
+	setExpectedXPSRflags("Nz");
+	setExpectedRegisterValue(R7, 0x77777777U * 0x77777777U);
+	step();
+}
+
+TEST_F(CpuTestHarness, mul_UseDifferentRegistersForEachArg)
+{
+	emitInstruction16("0100001101nnnddd", R1, R2);
+	setRegisterValue(R1, 0xA5A5);
+	setRegisterValue(R2, 2);
+	setExpectedXPSRflags("nz");
+	setExpectedRegisterValue(R2, 0xA5A5U << 1U);
+	step();
+}
+
+TEST_F(CpuTestHarness, mul_MultiplyBy16BitMaximumValues)
+{
+	emitInstruction16("0100001101nnnddd", R1, R2);
+	setRegisterValue(R1, 0xFFFF);
+	setRegisterValue(R2, 0xFFFF);
+	setExpectedXPSRflags("Nz");
+	setExpectedRegisterValue(R2, 0xFFFE0001);
+	step();
+}
