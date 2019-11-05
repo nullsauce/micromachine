177bc61b7836c118b8a9761f4be34ed4dc2a57ab
Flavio Roth <flavio.roth@gmail.com>
2019-11-05 18:52:48 +0000

    Disable disasm tests temporarly

--- test/disasm/disasm.cpp
+++ test/disasm/disasm.cpp
@@ -17,9 +17,9 @@ TEST(Disasm, DisamRegressionOf16bitInstructions)
 	char buf[128] = {0};
 	int i = 0;
 	for(i = 0; i < 0x10000; i++) {
-		instruction_pair instr((uint16_t)i, 0);
-		std::string str = disasm::disassemble_instruction(instr, 64);
-		sprintf(buf, "%04x %s", (uint16_t)instr.first(), str.c_str());
+		instruction_pair instruction((uint16_t)i, 0);
+		std::string str = disasm::disassemble_instruction(instruction, 64);
+		sprintf(buf, "%04x %s", (uint16_t)instruction.first(), str.c_str());
 		//truth << buf << std::endl;
 		EXPECT_TRUE(std::getline(truth, line));
 		EXPECT_STREQ(buf, line.c_str());
