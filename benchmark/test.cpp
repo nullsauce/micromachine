0f8ee0e78ed6e640f02e24796a544a489855adc1
Flavio Roth <flavio.roth@gmail.com>
2019-11-06 15:44:24 +0100

    Add support for google benchmark

---
+++ benchmark/test.cpp
@@ -0,0 +1,10 @@
+
+
+#include <benchmark/benchmark.h>
+
+static void BM_StringCreation(benchmark::State& state) {
+  for (auto _ : state)
+    std::string empty_string;
+}
+// Register the function as a benchmark
+BENCHMARK(BM_StringCreation);
