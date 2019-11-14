7cdf21554f212d53d9a0d33e44e0b3b7ed962ae9
Flavio Roth <flavio.roth@gmail.com>
2019-11-14 11:22:38 +0100

    termui debugger wip

--- termdbg/main.cpp
+++ termdbg/main.cpp
@@ -11,32 +11,37 @@
 #include <cppurses/widget/layouts/vertical.hpp>
 #include <cppurses/widget/widgets/menu.hpp>
 #include <cppurses/system/focus.hpp>
-#include <cppurses/widget/layouts/vertical.hpp>
+#include <cppurses/widget/layouts/horizontal.hpp>
 #include <cppurses/widget/widgets/menu_stack.hpp>
 #include <cppurses/widget/widgets/titlebar.hpp>
+#include <cppurses/widget/widgets/text_display.hpp>
 
-class Main_menu : public cppurses::layout::Vertical {
+class DisasmView : public cppurses::layout::Vertical {
 public:
-	cppurses::Titlebar& titlebar{this->make_child<cppurses::Titlebar>("  C  P  P  U  R  S  E  S")};
-	cppurses::Menu_stack& main_menu{this->make_child<cppurses::Menu_stack>("Demos")};
+	cppurses::Text_display& disasm_text{this->make_child<cppurses::Text_display>()};
+	DisasmView() {
+		disasm_text.set_alignment(cppurses::Alignment::Left);
+		disasm_text.set_contents("nothing");
+		disasm_text.width_policy.minimum(20);
+		disasm_text.width_policy.maximum(40);
+		disasm_text.border.enable();
+	}
+};
+
+class Main_menu : public cppurses::layout::Horizontal {
+public:
+	DisasmView& disasm_view{this->make_child<DisasmView>()};
 
 	Main_menu() {
 		this->focus_policy = cppurses::Focus_policy::Direct;
 		this->set_name("Main_menu - head widget");
-		titlebar.set_name("Titlebar in Main_menu");
-		main_menu.set_name("Menu_stack in Main_menu");
-		main_menu.menu().set_name("Menu in Main Widget Stack Menu");
-
-		auto& esc_short = cppurses::Shortcuts::add_shortcut(cppurses::Key::Escape);
-		esc_short.connect([this] { main_menu.goto_menu(); });
 	}
 };
 
 int main(int argc, const char** argv) {
-	setlocale(LC_ALL, "");
+
 	cppurses::System sys;
 	Main_menu demo_menu;
-	cppurses::System::set_initial_focus(&demo_menu.main_menu);
+	cppurses::System::set_initial_focus(&demo_menu.disasm_view);
 	return sys.run(demo_menu);
-
 }
