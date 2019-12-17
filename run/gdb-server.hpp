b7a2f82c6e234064ca3c08b235077c292a29e9f5
Flavio Roth <flavio.roth@gmail.com>
2019-12-17 19:32:29 +0000

    Rudimentary debug symbols loading

--- run/gdb-server.hpp
+++ run/gdb-server.hpp
@@ -19,9 +19,14 @@ and/or distributed without the express permission of Flavio Roth.
 #include <cstring>
 #include <regex>
 #include <cpu.hpp>
+#include <stdarg.h>
+
+#include "programmer.hpp"
+
+#define GDB_SERVER_MAX_PACKET_SIZE 2048
 
 namespace {
-	static uint8_t compute_checksum(const std::string& data) {
+	static uint8_t compute_checksum(const std::vector<uint8_t>& data) {
 		uint64_t computed_checksum = 0;
 		for(char c : data) {
 			computed_checksum += c;
@@ -40,9 +45,44 @@ public:
 	{}
 
 	void send_ack() {
-		send(_socket, "+45", 3, 0);
+		send(_socket, "+", 1, 0);
 	}
 
+	void send_packet_binary(const uint8_t* data, size_t data_size) {
+		const size_t size_of_checksum = 2;
+		const size_t size_of_separators = 2;
+		if(data_size > GDB_SERVER_MAX_PACKET_SIZE - (size_of_checksum + size_of_separators)) {
+			fprintf(stderr, "Error: data is too large!");
+			return;
+		}
+		static std::vector<uint8_t> packet_buffer(GDB_SERVER_MAX_PACKET_SIZE);
+		std::vector<uint8_t> packet_data = escape(data, data_size);
+		uint8_t checksum = compute_checksum(packet_data);
+		packet_buffer.clear();
+		packet_buffer.push_back('$');
+		packet_buffer.insert(packet_buffer.end(), packet_data.begin(), packet_data.end());
+		packet_buffer.push_back('#');
+		char checksum_str[3];
+		snprintf(checksum_str, 3, "%02x", checksum);
+		packet_buffer.push_back(checksum_str[0]);
+		packet_buffer.push_back(checksum_str[1]);
+		send(_socket, packet_buffer.data(), packet_buffer.size(), 0);
+		fprintf(stderr, "sent: %s\n", packet_buffer.data());
+	}
+
+	void send_packet(const char* data, ...) {
+		static uint8_t packet_buffer[2048];
+		va_list argptr;
+		va_start(argptr, data);
+		size_t size = vsprintf((char*)packet_buffer, data, argptr);
+		va_end(argptr);
+		send_packet_binary(packet_buffer, size);
+	}
+
+	void send_packet(const std::string& data) {
+		return send_packet(data.c_str());
+	}
+	/*
 	void send_packet(const std::string& data) {
 		static char packet_buffer[2048];
 		std::string packet_data = escape(data);
@@ -50,29 +90,39 @@ public:
 		int packet_size = snprintf(packet_buffer, sizeof(packet_buffer), "$%s#%02x", packet_data.c_str(), checksum);
 		send(_socket, packet_buffer, packet_size, 0);
 		fprintf(stderr, "sent: %s\n", packet_buffer);
-	}
+	}*/
 
-	std::string escape(const std::string& data) {
-		std::stringbuf ss;
-		for(char c : data) {
-			if(c == '$' || c == '#' || c == '}') {
-				ss.sputc('}');
-				ss.sputc(c ^ 0x20);
+	std::vector<uint8_t> escape(const uint8_t* data, size_t size) {
+		std::vector<uint8_t> escaped;
+		escaped.reserve(size*2);
+
+		for(size_t i = 0; i < size; i++) {
+			char c = data[i];
+			if(c == '$' || c == '#' || c == '*'  || c == '}') {
+				escaped.push_back('}');
+				escaped.push_back(c ^ 0x20);
 			} else {
-				ss.sputc(c);
+				escaped.push_back(c);
 			}
 		}
-		return ss.str();
+		return escaped;
+	}
+
+	std::vector<uint8_t> escape(const std::string& data) {
+		return escape((uint8_t*)data.data(), data.size());
 	}
 };
 
 class gdb_server {
 private:
 	cpu& _cpu;
+	programmer::program::ptr _program;
 
 public:
-	gdb_server(cpu& cpu)
-		: _cpu(cpu) {
+
+	gdb_server(cpu& cpu, programmer::program::ptr program)
+		: _cpu(cpu)
+		, _program(program) {
 			// Creating socket file descriptor
 			int server_fd = 0;
 			if((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
@@ -104,7 +154,7 @@ public:
 
 		client_stream client(client_socket);
 		std::string matchbuff;
-		static char recvbuf[1024];
+		static char recvbuf[GDB_SERVER_MAX_PACKET_SIZE];
 		while(true) {
 			int received = read(client_socket , recvbuf, sizeof(recvbuf));
 			if(received == 0) {
@@ -126,7 +176,7 @@ public:
 				} else if(whole_match.compare("\x03") == 0) {
 					// CTRL+C
 					fprintf(stderr, "User requested exit\n");
-					client.send_packet("O48656c6c6f2c20776f726c64210a");
+					client.send_packet("476f6f64627965210a");
 				} else {
 					std::string data = m[4];
 					std::string checksum = m[5];
@@ -145,8 +195,10 @@ public:
 	}
 
 	void handle_packet(const std::string& packet, client_stream& client) {
+		std::smatch smatch;
 		if(packet.rfind("qSupported:", 0) == 0) {
-			client.send_packet("PacketSize=1024");
+			client.send_packet("PacketSize=800;vContSupported;qXfer:exec-file:read+");
+			//client.send_packet("QStartNoAckMode");
 		} else if(packet.rfind("vMustReplyEmpty", 0) == 0) {
 			client.send_packet("");
 		} else if(packet[0] == 'H') {
@@ -156,6 +208,19 @@ public:
 			client.send_packet("OK");
 		} else if(packet[0] == 'c') {
 			client.send_packet("OK");
+		} else if(packet[0] == 'Z') {
+			client.send_packet("");
+		} else if(packet[0] == 'X') {
+			uint32_t address = 0;
+			uint32_t size = 0;
+			sscanf(packet.data(), "%u,%u:", &address, &size);
+			fprintf(stderr, "write memory address=%u, size=%u\n", address, size);
+			size_t data_begin = packet.find(':') + 1;
+			for(size_t i = 0; i < size; i++) {
+				_cpu.mem().write8(address + i, packet.data()[data_begin + i]);
+			}
+			client.send_packet("OK");
+
 		} else if(packet.compare("qTStatus") == 0) {
 			client.send_packet("");
 		} else if(packet.compare("vCont?") == 0) {
@@ -172,6 +237,10 @@ public:
 			client.send_packet("1");
 		} else if(packet.compare("qC") == 0) {
 			client.send_packet("QC0");
+		} else if(packet.compare("qOffsets") == 0) {
+			client.send_packet("TextSeg=0");
+		} else if(packet.compare("qSymbol::") == 0) {
+			client.send_packet("OK");
 		} else if(packet.compare("g") == 0) {
 			static char buf[16*8+1];
 			sprintf(buf, "%08X%08X%08X%08X%08X%08X%08X%08X%08X%08X%08X%08X%08X%08X%08X%08X",
@@ -221,8 +290,36 @@ public:
 					client.send_packet("E2");
 				}
 			}
-		} else {
+		} else if(packet.rfind("qXfer:exec-file:read:", 0) == 0) {
+			client.send_packet("l/program.elf");
+		} else if(packet.rfind("vFile:setfs:", 0) == 0) {
 			client.send_packet("");
+		} else if(packet.rfind("vFile:fstat", 0) == 0) {
+			client.send_packet("");
+		} else if(packet.rfind("vFile:open:6a7573742070726f62696e67", 0) == 0) {
+			// fake open requested used for probing
+			client.send_packet("F-1");
+		} else if(packet.rfind("vFile:open:2f70726f6772616d2e656c66", 0) == 0) {
+			// open request for /program.elf
+			client.send_packet("F0"); // return virtual file descriptor '0'
+		} else if(std::regex_match(packet, smatch, std::regex("vFile:pread:0,([a-f0-9]+),([a-f0-9]+)"))) {
+			// read only implemented for virtual file descriptor '0'
+			size_t offset = 0;
+			size_t size = 0;
+			sscanf(smatch[1].str().c_str(), "%zx", &size);
+			sscanf(smatch[2].str().c_str(), "%zx", &offset);
+			fprintf(stderr, "read file descriptor=0, offset=%zx, size=%zx\n", offset, size);
+
+			if(offset + size > _program->size()) {
+				size = _program->size() - offset;
+			}
+
+			static uint8_t packet_buff[1024];
+			size_t data_size = std::min(size, sizeof(packet_buff) - 14);
+			size_t header_size = sprintf((char*)packet_buff, "F%zX;", data_size);
+			memcpy(packet_buff + header_size, _program->data() + offset, data_size);
+			client.send_packet_binary(packet_buff, header_size + data_size);
+
 		}
 	}
 
