952d5cb6bcb8a91c23d232b358e417a2d4370ec4
Flavio Roth <flavio.roth@gmail.com>
2020-04-08 21:44:50 +0200

    Better integration of ccpurses

---
+++ cmake/cppurses.cmake
@@ -0,0 +1,14 @@
+include(FetchContent)
+
+# this requires a recent cmake
+FetchContent_Declare(
+	cppurses
+	GIT_REPOSITORY https://github.com/a-n-t-h-o-n-y/CPPurses
+	GIT_TAG "791dfe5eb696d2729c80f7c45d18620bcaa78b5f"
+)
+
+FetchContent_GetProperties(cppurses)
+if(NOT cppurses_POPULATED)
+  FetchContent_Populate(cppurses)
+  add_subdirectory(${CMAKE_SOURCE_DIR}/external/cppurses  ${CMAKE_BINARY_DIR}/external/cppurses)
+endif()
