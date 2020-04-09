df898d51a92fa4fee109cdf4de33bb83d6e752ed
Flavio Roth <flavio.roth@gmail.com>
2020-04-09 17:17:35 +0200

    Fixes bogus headers

--- micromachine/exception_defs.hpp
+++ micromachine/exception_defs.hpp
@@ -1,9 +1,9 @@
-/* Copyright (C) The Micromachine project - 2016 - All Rights Reserved
+/* Copyright (C) Flavio Roth - All Rights Reserved
 
 * Unauthorized copying of this file, via any medium is strictly prohibited.
 
 * This project is proprietary and confidential. It can not be copied
-and/or distributed without the express permission of The Micromachine project.
+and/or distributed without the express permission of Flavio Roth.
 
 */
 
