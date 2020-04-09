dcf9fc430d57e57f6dfc10b3630d1027a3f8b5a2
Flavio Roth <flavio.roth@gmail.com>
2020-04-09 17:17:35 +0200

    Fixes bogus headers

--- micromachine/interrupter.hpp
+++ micromachine/interrupter.hpp
@@ -1,9 +1,9 @@
-/* Copyright (C) MindMaze SA - 2016 - All Rights Reserved
+/* Copyright (C) Flavio Roth - All Rights Reserved
 
 * Unauthorized copying of this file, via any medium is strictly prohibited.
 
 * This project is proprietary and confidential. It can not be copied
-and/or distributed without the express permission of MindMaze SA.
+and/or distributed without the express permission of Flavio Roth.
 
 */
 
