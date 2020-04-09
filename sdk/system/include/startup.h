d3b697c76d86a2cd60ac188db39292d1edf88944
Flavio Roth <flavio.roth@gmail.com>
2020-04-09 22:12:37 +0200

    Moves all memory layout definitions in system.h

---
+++ sdk/system/include/startup.h
@@ -0,0 +1,16 @@
+/* Copyright (C) Flavio Roth - All Rights Reserved
+
+* Unauthorized copying of this file, via any medium is strictly prohibited.
+
+* This project is proprietary and confidential. It can not be copied
+and/or distributed without the express permission of Flavio Roth.
+
+*/
+
+#ifndef MICROMACHINE_STARTUP_H
+#define MICROMACHINE_STARTUP_H
+
+void _startup();
+
+
+#endif //MICROMACHINE_STARTUP_H
