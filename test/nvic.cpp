0192808e8cd7a464429820e331b206f7d008c6ba
Flavio Roth <flavio.roth@gmail.com>
2020-04-09 12:51:57 +0200

    Provides proper public accessors to nvic. Clarifies a few things with code documentation.

--- test/nvic.cpp
+++ test/nvic.cpp
@@ -7,123 +7,109 @@
 
 TEST(NVICRegisters, ISER_ReadIsCoherent)
 {
-	nvic nv;
-	const uint32_t initial_enable_status = 0b1111011100110001;
-	nvic::tester(&nv).enable_status() = initial_enable_status;
-	EXPECT_EQ(nv._iser_reg, initial_enable_status);
+	const uint32_t initial_enable_statuses = 0b1111011100110001;
+	nvic nv = nvic::with_enable_statuses(initial_enable_statuses);
+	EXPECT_EQ(nv.iser_reg(), initial_enable_statuses);
 }
 
 TEST(NVICRegisters, ISER_WriteZeroHasNoEffect)
 {
-	nvic nv;
-	const uint32_t initial_enable_status = 0b1111011100110001;
-	nvic::tester(&nv).enable_status() = initial_enable_status;
-	nv._iser_reg = 0;
-	EXPECT_EQ(nv._iser_reg, initial_enable_status);
+	const uint32_t initial_enable_statuses = 0b1111011100110001;
+	nvic nv = nvic::with_enable_statuses(initial_enable_statuses);
+	nv.iser_reg() = 0;
+	EXPECT_EQ(nv.iser_reg(), initial_enable_statuses);
 }
 
 TEST(NVICRegisters, ISER_WriteOneSetsEnabledStatus)
 {
-	nvic nv;
-	nvic::tester(&nv).enable_status() = 0b1111011100110001;
-	nv._iser_reg = 0b0000000000000010;
-	EXPECT_EQ(nv._iser_reg, 0b1111011100110011);
+	nvic nv = nvic::with_enable_statuses(0b1111011100110001);
+	nv.iser_reg() = 0b0000000000000010;
+	EXPECT_EQ(nv.iser_reg(), 0b1111011100110011);
 }
 
 TEST(NVICRegisters, ICER_ReadIsCoherent)
 {
-	nvic nv;
-	const uint32_t initial_enable_status = 0b1111011100110001;
-	nvic::tester(&nv).enable_status() = initial_enable_status;
-	EXPECT_EQ(nv._icer_reg, initial_enable_status);
+	const uint32_t initial_enable_statuses = 0b1111011100110001;
+	nvic nv = nvic::with_enable_statuses(initial_enable_statuses);
+	EXPECT_EQ(nv.icer_reg(), initial_enable_statuses);
 }
 
 TEST(NVICRegisters, ICER_WriteZeroHasNoEffect)
 {
-	nvic nv;
-	const uint32_t initial_enable_status = 0b1111011100110001;
-	nvic::tester(&nv).enable_status() = initial_enable_status;
-	nv._icer_reg = 0;
-	EXPECT_EQ(nv._icer_reg, initial_enable_status);
+	const uint32_t initial_enable_statuses = 0b1111011100110001;
+	nvic nv = nvic::with_enable_statuses(initial_enable_statuses);
+	nv.icer_reg() = 0;
+	EXPECT_EQ(nv.icer_reg(), initial_enable_statuses);
 }
 
 TEST(NVICRegisters, ICER_WriteOneClearsEnabledStatus)
 {
-	nvic nv;
-	nvic::tester(&nv).enable_status() = 0b1111011100110001;
-	nv._icer_reg = 0b0000000000000001;
-	EXPECT_EQ(nv._icer_reg, 0b1111011100110000);
+	nvic nv = nvic::with_enable_statuses(0b1111011100110001);
+	nv.icer_reg() = 0b0000000000000001;
+	EXPECT_EQ(nv.icer_reg(), 0b1111011100110000);
 }
 
 TEST(NVICRegisters, ICER_ISER_ReadsAreCoherent)
 {
-	nvic nv;
-	const uint32_t initial_enable_status = 0b1111011100110001;
-	nvic::tester(&nv).enable_status() = initial_enable_status;
+	const uint32_t initial_enable_statuses = 0b1111011100110001;
+	nvic nv = nvic::with_enable_statuses(initial_enable_statuses);
 	// disable interrupt 0
-	nv._icer_reg = 0b0000000000000001;
-	EXPECT_EQ(nv._icer_reg, 0b1111011100110000);
-	EXPECT_EQ(nv._iser_reg, nv._icer_reg);
+	nv.icer_reg() = 0b0000000000000001;
+	EXPECT_EQ(nv.icer_reg(), 0b1111011100110000);
+	EXPECT_EQ(nv.iser_reg(), nv.icer_reg());
 }
 
 
 TEST(NVICRegisters, ISPR_ReadIsCoherent)
 {
-	nvic nv;
-	const uint32_t initial_pending_status = 0b1111011100110001;
-	nvic::tester(&nv).pending_status() = initial_pending_status;
-	EXPECT_EQ(nv._ispr_reg, initial_pending_status);
+	const uint32_t initial_pending_statuses = 0b1111011100110001;
+	nvic nv = nvic::with_pending_statuses(initial_pending_statuses);
+	EXPECT_EQ(nv.ispr_reg(), initial_pending_statuses);
 }
 
 TEST(NVICRegisters, ISPR_WriteZeroHasNoEffect)
 {
-	nvic nv;
-	const uint32_t initial_pending_status = 0b1111011100110001;
-	nvic::tester(&nv).pending_status() = initial_pending_status;
-	nv._ispr_reg = 0;
-	EXPECT_EQ(nv._ispr_reg, initial_pending_status);
+	const uint32_t initial_pending_statuses = 0b1111011100110001;
+	nvic nv = nvic::with_pending_statuses(initial_pending_statuses);
+	nv.ispr_reg() = 0;
+	EXPECT_EQ(nv.ispr_reg(), initial_pending_statuses);
 }
 
 TEST(NVICRegisters, ISPR_WriteOneSetsEnabledStatus)
 {
-	nvic nv;
-	nvic::tester(&nv).pending_status() = 0b1111011100110001;
-	nv._ispr_reg = 0b0000000000000010;
-	EXPECT_EQ(nv._ispr_reg, 0b1111011100110011);
+	nvic nv = nvic::with_pending_statuses(0b1111011100110001);
+	nv.ispr_reg() = 0b0000000000000010;
+	EXPECT_EQ(nv.ispr_reg(), 0b1111011100110011);
 }
 
 TEST(NVICRegisters, ICPR_ReadIsCoherent)
 {
-	nvic nv;
-	const uint32_t initial_pending_status = 0b1111011100110001;
-	nvic::tester(&nv).pending_status() = initial_pending_status;
-	EXPECT_EQ(nv._icpr_reg, initial_pending_status);
+	const uint32_t initial_pending_statuses = 0b1111011100110001;
+	nvic nv = nvic::with_pending_statuses(initial_pending_statuses);
+	EXPECT_EQ(nv.icpr_reg(), initial_pending_statuses);
 }
 
 TEST(NVICRegisters, ICPR_WriteZeroHasNoEffect)
 {
-	nvic nv;
-	const uint32_t initial_pending_status = 0b1111011100110001;
-	nvic::tester(&nv).pending_status() = initial_pending_status;
-	nv._icpr_reg = 0;
-	EXPECT_EQ(nv._icpr_reg, initial_pending_status);
+	const uint32_t initial_pending_statuses = 0b1111011100110001;
+	nvic nv = nvic::with_pending_statuses(initial_pending_statuses);
+	nv.icpr_reg() = 0;
+	EXPECT_EQ(nv.icpr_reg(), initial_pending_statuses);
 }
 
 TEST(NVICRegisters, ICPR_WriteOneClearsEnabledStatus)
 {
-	nvic nv;
-	nvic::tester(&nv).pending_status() = 0b1111011100110001;
-	nv._icpr_reg = 0b0000000000000001;
-	EXPECT_EQ(nv._icpr_reg, 0b1111011100110000);
+	nvic nv = nvic::with_pending_statuses(0b1111011100110001);
+	nv.icpr_reg() = 0b0000000000000001;
+	EXPECT_EQ(nv.icpr_reg(), 0b1111011100110000);
 }
 
 TEST(NVICRegisters, ICPR_ISPR_ReadsAreCoherent)
 {
-	nvic nv;
-	const uint32_t initial_pending_status = 0b1111011100110001;
-	nvic::tester(&nv).pending_status() = initial_pending_status;
+	const uint32_t initial_pending_statuses = 0b1111011100110001;
+	nvic nv = nvic::with_pending_statuses(initial_pending_statuses);
 	// disable interrupt 0
-	nv._icpr_reg = 0b0000000000000001;
-	EXPECT_EQ(nv._icpr_reg, 0b1111011100110000);
-	EXPECT_EQ(nv._ispr_reg, nv._icpr_reg);
+	nv.icpr_reg() = 0b0000000000000001;
+	EXPECT_EQ(nv.icpr_reg(), 0b1111011100110000);
+	EXPECT_EQ(nv.ispr_reg(), nv.icpr_reg());
 }
