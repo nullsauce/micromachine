52aa621670299f4decccbd22e21dd3754d1064f4
Flavio Roth <flavio.roth@gmail.com>
2020-04-11 21:35:12 +0200

    Exposes .data section properties in system

--- sdk/system/system.c
+++ sdk/system/system.c
@@ -17,6 +17,10 @@ extern char __bss_start__;
 extern char __bss_end__;
 extern char __bss_size__;
 
+extern char __data_start__;
+extern char __data_end__;
+extern char __data_size__;
+
 void* _system_heap_start = &__heap_start;
 void* _system_heap_end = &__heap_end;
 uint32_t _system_heap_size = (uint32_t)&__heap_size;
@@ -24,3 +28,7 @@ uint32_t _system_heap_size = (uint32_t)&__heap_size;
 void* _system_bss_start = &__bss_start__;
 void* _system_bss_end = &__bss_end__;
 uint32_t _system_bss_size = (uint32_t)&__bss_size__;
+
+void* _system_data_start = &__data_start__;
+void* _system_data_end = &__data_end__;
+uint32_t _system_data_size = (uint32_t)&__data_size__;
