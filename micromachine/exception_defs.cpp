90c2fe8652282c8505064ad35b7ee12a4f52e201
Flavio Roth <flavio.roth@gmail.com>
2020-04-15 13:34:22 +0200

    Remove unnecessary definitions

--- micromachine/exception_defs.cpp
+++
@@ -1,9 +0,0 @@
-
-
-#include "exception_defs.hpp"
-
-const exception::priority_t exception::RESET_PRIORITY;
-const exception::priority_t exception::NMI_PRIORITY;
-const exception::priority_t exception::HARDFAULT_PRIORITY;
-const exception::priority_t exception::THREAD_MODE_PRIORITY;
-
