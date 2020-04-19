eeee441910fdf8ddfc548d2cd30b01798fea2a5c
Flavio Roth <flavio.roth@gmail.com>
2020-04-19 17:19:07 +0200

    Test framework cleanup

--- test/framework/RegisterMutationPredicate.cpp
+++ test/framework/RegisterMutationPredicate.cpp
@@ -8,3 +8,23 @@ and/or distributed without the express permission of Flavio Roth.
 */
 
 #include "RegisterMutationPredicate.hpp"
+#include <gtest/gtest.h>
+
+RegisterMutationPredicate::RegisterMutationPredicate(const cpu& previous, const cpu& current, reg_idx regIdx)
+	: CpuMutationPredicate(previous, current)
+	, _regIdx(regIdx) {}
+
+RegisterMutationPredicate& RegisterMutationPredicate::Equals(uint32_t value) {
+	EXPECT_EQ(value, currentRegisterValue());
+	return *this;
+}
+
+RegisterMutationPredicate& RegisterMutationPredicate::DidNotChange() {
+	EXPECT_EQ(previousRegisterValue(), currentRegisterValue());
+	return *this;
+}
+
+RegisterMutationPredicate& RegisterMutationPredicate::Changed() {
+	EXPECT_NE(previousRegisterValue(), currentRegisterValue());
+	return *this;
+}
