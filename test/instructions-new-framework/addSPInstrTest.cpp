2e2977efe590784b4fdd855a4c89fd304bb91a07
Flavio Roth <flavio.roth@gmail.com>
2020-04-19 18:56:32 +0200

    Migrates add sp instruction to new test framework.

--- test/instructions-new-framework/addSPInstrTest.cpp
+++ test/instructions-new-framework/addSPInstrTest.cpp
@@ -11,56 +11,69 @@
     GNU General Public License for more details.
 */
 
-#include "CpuTestHarness.hpp"
+#include "CpuTestFixture.hpp"
 
 
 /* ADD SP Plus Immediate - Encoding T1
    Encoding: 1010 1 Rd:3 Imm:8 */
-TEST_F(CpuTestHarness, addSP_T1UseHighestRegisterAddSmallestImmediate)
-{
+MICROMACHINE_TEST_F(addSP, T1UseHighestRegisterAddSmallestImmediate, CpuTestFixture) {
+	static uint32_t INITIAL_SP = 0x00002000;
+	getCpu().regs().set(registers::SP, INITIAL_SP);
+
 	code_gen().emit_ins16("10101dddiiiiiiii", registers::R7, 0);
-	setExpectedRegisterValue(registers::R7, INITIAL_SP + 0);
-	step();
+	Step();
+	ExpectThat().Register(registers::R7).Equals(INITIAL_SP + 0);
 }
 
-TEST_F(CpuTestHarness, addSP_T1UseLowestRegisterAddLargestImmediate)
-{
+MICROMACHINE_TEST_F(addSP, T1UseLowestRegisterAddLargestImmediate, CpuTestFixture) {
+	static uint32_t INITIAL_SP = 0x00002000;
+	getCpu().regs().set(registers::SP, INITIAL_SP);
+
 	code_gen().emit_ins16("10101dddiiiiiiii", registers::R0, 255);
-	setExpectedRegisterValue(registers::R0, INITIAL_SP + 255 * 4);
-	step();
+	Step();
+	ExpectThat().Register(registers::R0).Equals(INITIAL_SP + 255 * 4);
 }
 
-TEST_F(CpuTestHarness, addSP_T1UseIntermediateValues)
-{
+MICROMACHINE_TEST_F(addSP, T1UseIntermediateValues, CpuTestFixture) {
+	static uint32_t INITIAL_SP = 0x00002000;
+	getCpu().regs().set(registers::SP, INITIAL_SP);
+
 	code_gen().emit_ins16("10101dddiiiiiiii", registers::R3, 128);
-	setExpectedRegisterValue(registers::R3, INITIAL_SP + 128 * 4);
-	step();
+	Step();
+	ExpectThat().Register(registers::R3).Equals(INITIAL_SP + 128 * 4);
 }
-
-
 
 /* ADD SP Plus Immediate - Encoding T2
    Encoding: 1011 0000 0 Imm:7 */
-TEST_F(CpuTestHarness, addSP_T2SmallestImmediate)
-{
+MICROMACHINE_TEST_F(addSP, T2SmallestImmediate, CpuTestFixture) {
+	static uint32_t INITIAL_PC = 0x00001000;
+	getCpu().regs().set_pc(INITIAL_PC);
+	getCpu().regs().set(registers::SP, INITIAL_PC + 1024);
+
 	code_gen().emit_ins16("101100000iiiiiii", 0);
-	setRegisterValue(registers::SP, INITIAL_PC + 1024);
-	setExpectedRegisterValue(registers::SP, INITIAL_PC + 1024 + 0);
-	step();
+
+	Step();
+	ExpectThat().Register(registers::SP).Equals(INITIAL_PC + 1024 + 0);
 }
 
-TEST_F(CpuTestHarness, addSP_T2LargestImmediate)
-{
+MICROMACHINE_TEST_F(addSP, T2LargestImmediate, CpuTestFixture) {
+	static uint32_t INITIAL_PC = 0x00001000;
+	getCpu().regs().set_pc(INITIAL_PC);
+	getCpu().regs().set(registers::SP, INITIAL_PC + 1024);
+
 	code_gen().emit_ins16("101100000iiiiiii", 127);
-	setRegisterValue(registers::SP, INITIAL_PC + 1024);
-	setExpectedRegisterValue(registers::SP, INITIAL_PC + 1024 + 127 * 4);
-	step();
+
+	Step();
+	ExpectThat().Register(registers::SP).Equals(INITIAL_PC + 1024 + 127 * 4);
 }
 
-TEST_F(CpuTestHarness, addSP_T2IntermediateValues)
-{
+MICROMACHINE_TEST_F(addSP, T2IntermediateValues, CpuTestFixture) {
+	static uint32_t INITIAL_PC = 0x00001000;
+	getCpu().regs().set_pc(INITIAL_PC);
+	getCpu().regs().set(registers::SP, INITIAL_PC + 1024);
+
 	code_gen().emit_ins16("101100000iiiiiii", 64);
-	setRegisterValue(registers::SP, INITIAL_PC + 1024);
-	setExpectedRegisterValue(registers::SP, INITIAL_PC + 1024 + 64 * 4);
-	step();
+
+	Step();
+	ExpectThat().Register(registers::SP).Equals(INITIAL_PC + 1024 + 64 * 4);
 }
