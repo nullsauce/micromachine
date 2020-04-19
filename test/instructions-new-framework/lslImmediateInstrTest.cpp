69ba42ad9b18c534791ef4e32f6b8a958b0dc8d4
Flavio Roth <flavio.roth@gmail.com>
2020-04-19 22:40:11 +0200

    Migrates lsl immediate instruction to new test framework.

--- test/instructions-new-framework/lslImmediateInstrTest.cpp
+++ test/instructions-new-framework/lslImmediateInstrTest.cpp
@@ -11,7 +11,7 @@
     GNU General Public License for more details.
 */
 
-#include "CpuTestHarness.hpp"
+#include "CpuTestFixture.hpp"
 
 // Immediate values used for shift amount in tests.
 #define IMM_0  0
@@ -23,63 +23,56 @@
 
 /* LSL - Immediate (Logical Shift Left)
    Encoding: 000 00 imm:5 Rm:3 Rd:3 */
-TEST_F(CpuTestHarness, lslImmediate_MovR7toR0_CarryUnmodified)
-{
+MICROMACHINE_TEST_F(lslImmediate, MovR7toR0_CarryUnmodified, CpuTestFixture) {
 	code_gen().emit_ins16("00000iiiiimmmddd", IMM_0, registers::R7, registers::R0);
-	setExpectedXPSRflags("nzc");
-	clearCarry();
-	setExpectedRegisterValue(registers::R0, 0x77777777U);
-	step();
+	getCpu().regs().app_status_register().write_carry_flag(false);
+	Step();
+	ExpectThat().XPSRFlagsEquals("nzc");
+	ExpectThat().Register(registers::R0).Equals(0x77777777U);
 }
 
-TEST_F(CpuTestHarness, lslImmediate_MovR0toR7_ZeroResultAndCarryUnmodified)
-{
+MICROMACHINE_TEST_F(lslImmediate, MovR0toR7_ZeroResultAndCarryUnmodified, CpuTestFixture) {
 	code_gen().emit_ins16("00000iiiiimmmddd", IMM_0, registers::R0, registers::R7);
-	setExpectedXPSRflags("nZC");
-	setCarry();
-	setExpectedRegisterValue(registers::R7, 0x0);
-	step();
+	getCpu().regs().app_status_register().write_carry_flag(true);
+	Step();
+	ExpectThat().XPSRFlagsEquals("nZC");
+	ExpectThat().Register(registers::R7).Equals(0x0);
 }
 
-TEST_F(CpuTestHarness, lslImmediate_ShiftR1by3_ResultInNegativeValue)
-{
+MICROMACHINE_TEST_F(lslImmediate, ShiftR1by3_ResultInNegativeValue, CpuTestFixture) {
 	code_gen().emit_ins16("00000iiiiimmmddd", IMM_3, registers::R1, registers::R0);
-	setExpectedXPSRflags("Nzc");
-	setExpectedRegisterValue(registers::R0, 0x11111111U << 3);
-	step();
+	Step();
+	ExpectThat().XPSRFlagsEquals("Nzc");
+	ExpectThat().Register(registers::R0).Equals(0x11111111U << 3);
 }
 
-TEST_F(CpuTestHarness, lslImmediate_ShiftR1by4_HasCarryOut)
-{
+MICROMACHINE_TEST_F(lslImmediate, ShiftR1by4_HasCarryOut, CpuTestFixture) {
 	code_gen().emit_ins16("00000iiiiimmmddd", IMM_4, registers::R1, registers::R0);
-	setExpectedXPSRflags("nzC");
-	setExpectedRegisterValue(registers::R0, 0x11111111U << 4);
-	step();
+	Step();
+	ExpectThat().Register(registers::R0).Equals(0x11111111U << 4);
+	ExpectThat().XPSRFlagsEquals("nzC");
 }
 
-TEST_F(CpuTestHarness, lslImmediate_ShiftR0by31_PushesLowestbitIntoSignBit)
-{
+MICROMACHINE_TEST_F(lslImmediate, ShiftR0by31_PushesLowestbitIntoSignBit, CpuTestFixture) {
 	code_gen().emit_ins16("00000iiiiimmmddd", IMM_31, registers::R0, registers::R0);
-	setExpectedXPSRflags("Nzc");
-	setRegisterValue(registers::R0, 1U);
-	setExpectedRegisterValue(registers::R0, 1U << 31);
-	step();
+	getCpu().regs().set(registers::R0, 1U);
+	Step();
+	ExpectThat().XPSRFlagsEquals("Nzc");
+	ExpectThat().Register(registers::R0).Equals(1U << 31);
 }
 
-TEST_F(CpuTestHarness, lslImmediate_CarryOutFromHighestBit)
-{
+MICROMACHINE_TEST_F(lslImmediate, CarryOutFromHighestBit, CpuTestFixture) {
 	code_gen().emit_ins16("00000iiiiimmmddd", IMM_1, registers::R0, registers::R0);
-	setExpectedXPSRflags("nzC");
-	setRegisterValue(registers::R0, 0xA0000000U);
-	setExpectedRegisterValue(registers::R0, 0xA0000000U << 1);
-	step();
+	getCpu().regs().set(registers::R0, 0xA0000000U);
+	Step();
+	ExpectThat().XPSRFlagsEquals("nzC");
+	ExpectThat().Register(registers::R0).Equals(0xA0000000U << 1);
 }
 
-TEST_F(CpuTestHarness, lslImmediate_CarryOutFromLowestBit)
-{
+MICROMACHINE_TEST_F(lslImmediate, CarryOutFromLowestBit, CpuTestFixture) {
 	code_gen().emit_ins16("00000iiiiimmmddd", IMM_31, registers::R0, registers::R0);
-	setExpectedXPSRflags("nZC");
-	setRegisterValue(registers::R0, 0x2U);
-	setExpectedRegisterValue(registers::R0, 0x2U << 31);
-	step();
+	getCpu().regs().set(registers::R0, 0x2U);
+	Step();
+	ExpectThat().XPSRFlagsEquals("nZC");
+	ExpectThat().Register(registers::R0).Equals(0x2U << 31);
 }
