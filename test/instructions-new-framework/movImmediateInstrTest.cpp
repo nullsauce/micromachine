9546cec637c0e4af6290ad01e619b4688d78d249
Flavio Roth <flavio.roth@gmail.com>
2020-04-19 23:25:14 +0200

    Migrates mov imm instruction to new test framework.

--- test/instructions-new-framework/movImmediateInstrTest.cpp
+++ test/instructions-new-framework/movImmediateInstrTest.cpp
@@ -11,43 +11,39 @@
     GNU General Public License for more details.
 */
 
-#include "CpuTestHarness.hpp"
+#include "CpuTestFixture.hpp"
 
 
 /* MOV - Immediate
    Encoding: 001 00 Rd:3 Imm:8 */
 /* NOTE: APSR_C state is maintained by this instruction. */
-TEST_F(CpuTestHarness, movImmediate_MovToR0)
-{
+MICROMACHINE_TEST_F(movImmediate, MovToR0, CpuTestFixture) {
 	code_gen().emit_ins16("00100dddiiiiiiii", registers::R0, 127);
 	// Use a couple of tests to explicitly set/clear carry to verify both states are maintained.
-	setExpectedXPSRflags("nzc");
-	clearCarry();
-	setExpectedRegisterValue(registers::R0, 127);
-	step();
+	getCpu().regs().app_status_register().write_carry_flag(false);
+	Step();
+	ExpectThat().Register(registers::R0).Equals(127);
+	ExpectThat().XPSRFlagsEquals("nzc");
 }
 
-TEST_F(CpuTestHarness, movImmediate_MovToR7)
-{
+MICROMACHINE_TEST_F(movImmediate, MovToR7, CpuTestFixture) {
 	code_gen().emit_ins16("00100dddiiiiiiii", registers::R7, 127);
-	setExpectedXPSRflags("nzC");
-	setCarry();
-	setExpectedRegisterValue(registers::R7, 127);
-	step();
+	getCpu().regs().app_status_register().write_carry_flag(true);
+	Step();
+	ExpectThat().XPSRFlagsEquals("nzC");
+	ExpectThat().Register(registers::R7).Equals(127);
 }
 
-TEST_F(CpuTestHarness, movImmediate_MovSmallestImmediateValueToR3)
-{
+MICROMACHINE_TEST_F(movImmediate, MovSmallestImmediateValueToR3, CpuTestFixture) {
 	code_gen().emit_ins16("00100dddiiiiiiii", registers::R3, 0);
-	setExpectedXPSRflags("nZ");
-	setExpectedRegisterValue(registers::R3, 0);
-	step();
+	Step();
+	ExpectThat().XPSRFlagsEquals("nZ");
+	ExpectThat().Register(registers::R3).Equals(0);
 }
 
-TEST_F(CpuTestHarness, movImmediate_MovLargestImmediateValueToR3)
-{
+MICROMACHINE_TEST_F(movImmediate, MovLargestImmediateValueToR3, CpuTestFixture) {
 	code_gen().emit_ins16("00100dddiiiiiiii", registers::R3, 255);
-	setExpectedXPSRflags("nz");
-	setExpectedRegisterValue(registers::R3, 255);
-	step();
+	Step();
+	ExpectThat().XPSRFlagsEquals("nz");
+	ExpectThat().Register(registers::R3).Equals(255);
 }
