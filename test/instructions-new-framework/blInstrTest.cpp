ea3b21ea191b6a9952ba2281165c2e1c3d630f93
Flavio Roth <flavio.roth@gmail.com>
2020-04-19 21:05:53 +0200

    Migrates bl instruction to new test framework.

--- test/instructions-new-framework/blInstrTest.cpp
+++ test/instructions-new-framework/blInstrTest.cpp
@@ -11,33 +11,36 @@
     GNU General Public License for more details.
 */
 
-#include "CpuTestHarness.hpp"
+#include "CpuTestFixture.hpp"
 
 
 /* BL (Branch with Link)
    Encoding: 11110 S Imm:10
              11 J1:1 1 J2:1 Imm:11
     Note: J1 and J2 are translated to immediate bits via I? = NOT(J? XOR S) */
-TEST_F(CpuTestHarness, bl_OffsetOf0)
-{
+MICROMACHINE_TEST_F(bl, OffsetOf0, CpuTestFixture) {
+	constexpr uint32_t INITIAL_PC = 0x00001000;
+	getCpu().regs().set_pc(INITIAL_PC);
 	code_gen().emit_ins32("11110Siiiiiiiiii", "11j1kiiiiiiiiiii", 0, 0, 1, 1, 0);
-	setExpectedRegisterValue(registers::PC, INITIAL_PC + 4);
-	setExpectedRegisterValue(registers::LR, (INITIAL_PC + 4) | 1);
-	step();
+	Step();
+	ExpectThat().Register(registers::PC).Equals(INITIAL_PC + 4);
+	ExpectThat().Register(registers::LR).Equals((INITIAL_PC + 4) | 1);
 }
 
-TEST_F(CpuTestHarness, bl_MaximumPositiveOffset)
-{
+MICROMACHINE_TEST_F(bl, MaximumPositiveOffset, CpuTestFixture) {
+	constexpr uint32_t INITIAL_PC = 0x00001000;
+	getCpu().regs().set_pc(INITIAL_PC);
 	code_gen().emit_ins32("11110Siiiiiiiiii", "11j1kiiiiiiiiiii", 0, 0x3FF, 0, 0, 0x7FF);
-	setExpectedRegisterValue(registers::PC, INITIAL_PC + 4 + 16777214);
-	setExpectedRegisterValue(registers::LR, (INITIAL_PC + 4) | 1);
-	step();
+	Step();
+	ExpectThat().Register(registers::PC).Equals(INITIAL_PC + 4 + 16777214);
+	ExpectThat().Register(registers::LR).Equals((INITIAL_PC + 4) | 1);
 }
 
-TEST_F(CpuTestHarness, bl_MaximumNegativeOffset)
-{
+MICROMACHINE_TEST_F(bl, MaximumNegativeOffset, CpuTestFixture) {
+	constexpr uint32_t INITIAL_PC = 0x00001000;
+	getCpu().regs().set_pc(INITIAL_PC);
 	code_gen().emit_ins32("11110Siiiiiiiiii", "11j1kiiiiiiiiiii", 1, 0, 0, 0, 0);
-	setExpectedRegisterValue(registers::PC, INITIAL_PC + 4 - 16777216);
-	setExpectedRegisterValue(registers::LR, (INITIAL_PC + 4) | 1);
-	step();
+	Step();
+	ExpectThat().Register(registers::PC).Equals(INITIAL_PC + 4 - 16777216);
+	ExpectThat().Register(registers::LR).Equals((INITIAL_PC + 4) | 1);
 }
