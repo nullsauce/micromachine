9daf718c125d34c22b2b17672cf3341ccc1b442d
Flavio Roth <flavio.roth@gmail.com>
2020-04-19 20:25:48 +0200

    Migrates bic instruction to new test framework.

--- test/instructions-new-framework/bicRegisterInstrTest.cpp
+++ test/instructions-new-framework/bicRegisterInstrTest.cpp
@@ -11,45 +11,46 @@
     GNU General Public License for more details.
 */
 
-#include "CpuTestHarness.hpp"
+#include "CpuTestFixture.hpp"
 
 
 /* BIC - Register
    Encoding: 010000 1110 Rm:3 Rdn:3 */
 /* NOTE: APSR_C state is maintained by this instruction. */
-TEST_F(CpuTestHarness, bicRegister_UseLowestRegisterForBothArgs)
-{
+MICROMACHINE_TEST_F(bicRegister, UseLowestRegisterForBothArgs, CpuTestFixture) {
 	code_gen().emit_ins16("0100001110mmmddd", registers::R0, registers::R0);
 	// Use a couple of tests to explicitly set/clear carry to verify both states are maintained.
-	setExpectedXPSRflags("nZc");
-	clearCarry();
-	setExpectedRegisterValue(registers::R0, 0);
-	step();
+
+	// clear carry flag
+	getCpu().regs().app_status_register().write_carry_flag(false);
+	Step();
+	ExpectThat().XPSRFlagsEquals("nZc");
+	ExpectThat().Register(registers::R0).Equals(0);
 }
 
-TEST_F(CpuTestHarness, bicRegister_UseHighestRegisterForBothArgs)
-{
+MICROMACHINE_TEST_F(bicRegister, UseHighestRegisterForBothArgs, CpuTestFixture) {
 	code_gen().emit_ins16("0100001110mmmddd", registers::R7, registers::R7);
-	setExpectedXPSRflags("nZC");
-	setCarry();
-	setExpectedRegisterValue(registers::R7, 0);
-	step();
+	// set carry flag
+	getCpu().regs().app_status_register().write_carry_flag(true);
+	Step();
+	ExpectThat().XPSRFlagsEquals("nZC");
+	ExpectThat().Register(registers::R7).Equals(0);
 }
 
-TEST_F(CpuTestHarness, bicRegister_UseR3andR7)
-{
+MICROMACHINE_TEST_F(bicRegister, UseR3andR7, CpuTestFixture) {
+	getCpu().regs().set(registers::R3, 0x33333333);
+	getCpu().regs().set(registers::R7, 0x77777777);
 	code_gen().emit_ins16("0100001110mmmddd", registers::R3, registers::R7);
-	setExpectedXPSRflags("nz");
-	setExpectedRegisterValue(registers::R7, 0x77777777 & ~0x33333333);
-	step();
+	Step();
+	ExpectThat().XPSRFlagsEquals("nz");
+	ExpectThat().Register(registers::R7).Equals(0x77777777 & ~0x33333333);
 }
 
-TEST_F(CpuTestHarness, bicRegister_UseBicToClearLSbit)
-{
+MICROMACHINE_TEST_F(bicRegister, UseBicToClearLSbit, CpuTestFixture) {
 	code_gen().emit_ins16("0100001110mmmddd", registers::R6, registers::R1);
-	setRegisterValue(registers::R1, -1);
-	setRegisterValue(registers::R6, 1);
-	setExpectedXPSRflags("Nz");
-	setExpectedRegisterValue(registers::R1, -1U & ~1);
-	step();
+	getCpu().regs().set(registers::R1, -1);
+	getCpu().regs().set(registers::R6, 1);
+	Step();
+	ExpectThat().XPSRFlagsEquals("Nz");
+	ExpectThat().Register(registers::R1).Equals(-1U & ~1);
 }
