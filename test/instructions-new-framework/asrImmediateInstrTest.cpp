a0a2ad68a4ae4ff3b803b6c68f1b81e8b6581aa2
Flavio Roth <flavio.roth@gmail.com>
2020-04-19 20:22:08 +0200

    Migrates asr imm instruction to new test framework.

--- test/instructions-new-framework/asrImmediateInstrTest.cpp
+++ test/instructions-new-framework/asrImmediateInstrTest.cpp
@@ -11,7 +11,7 @@
     GNU General Public License for more details.
 */
 
-#include "CpuTestHarness.hpp"
+#include "CpuTestFixture.hpp"
 
 
 // Immediate values used for shift amount in tests.
@@ -21,62 +21,64 @@
 
 /* ASR - Immediate (Arithmetic Shift Right)
    Encoding: 000 10 imm:5 Rm:3 Rd:3 */
-TEST_F(CpuTestHarness, asrImmediate_ShiftNegativeNumberBy1_Shift0OutFromLowestBit)
-{
+MICROMACHINE_TEST_F(asrImmediate, ShiftNegativeNumberBy1_Shift0OutFromLowestBit, CpuTestFixture) {
+	getCpu().regs().set(registers::R7, 0x77777777);
 	code_gen().emit_ins16("00010iiiiimmmddd", IMM_1, registers::R0, registers::R7);
-	setRegisterValue(registers::R0, 0x80000000U);
-	setExpectedXPSRflags("Nzc");
-	setExpectedRegisterValue(registers::R7, (int32_t) 0x80000000U >> 1);
-	step();
+	getCpu().regs().set(registers::R0, 0x80000000U);
+	Step();
+	ExpectThat().XPSRFlagsEquals("Nzc");
+	ExpectThat().Register(registers::R7).Equals((int32_t)0x80000000U >> 1);
 }
 
-TEST_F(CpuTestHarness, asrImmediate_ShiftPositiveNumberBy1_Shift1OutFromLowestBit)
-{
+MICROMACHINE_TEST_F(asrImmediate, ShiftPositiveNumberBy1_Shift1OutFromLowestBit, CpuTestFixture) {
+	getCpu().regs().set(registers::R7, 0x77777777);
+
 	code_gen().emit_ins16("00010iiiiimmmddd", IMM_1, registers::R7, registers::R0);
-	setRegisterValue(registers::R7, 0x7FFFFFFFU);
-	setExpectedXPSRflags("nzC");
-	setExpectedRegisterValue(registers::R0, (int32_t) 0x7FFFFFFFU >> 1);
-	step();
+	getCpu().regs().set(registers::R7, 0x7FFFFFFFU);
+	Step();
+	ExpectThat().XPSRFlagsEquals("nzC");
+	ExpectThat().Register(registers::R0).Equals((int32_t)0x7FFFFFFFU >> 1);
 }
 
-TEST_F(CpuTestHarness, asrImmediate_NegativeNumberBy32_Shift1OutFromHighestBit)
-{
+MICROMACHINE_TEST_F(asrImmediate, NegativeNumberBy32_Shift1OutFromHighestBit, CpuTestFixture) {
 	code_gen().emit_ins16("00010iiiiimmmddd", IMM_32, registers::R0, registers::R0);
-	setRegisterValue(registers::R0, 0x80000000U);
-	setExpectedXPSRflags("NzC");
-	setExpectedRegisterValue(registers::R0, 0xFFFFFFFFU);
-	step();
+	getCpu().regs().set(registers::R0, 0x80000000U);
+	Step();
+	ExpectThat().XPSRFlagsEquals("NzC");
+	ExpectThat().Register(registers::R0).Equals(0xFFFFFFFFU);
 }
 
-TEST_F(CpuTestHarness, asrImmediate_PositiveNumberBy32_Shift0FromHighestBit)
-{
+MICROMACHINE_TEST_F(asrImmediate, PositiveNumberBy32_Shift0FromHighestBit, CpuTestFixture) {
+	getCpu().regs().set(registers::R6, 0x66666666);
+	getCpu().regs().set(registers::R1, 0x11111111);
 	code_gen().emit_ins16("00010iiiiimmmddd", IMM_32, registers::R1, registers::R6);
-	setRegisterValue(registers::R1, 0x7FFFFFFFU);
-	setExpectedXPSRflags("nZc");
-	setExpectedRegisterValue(registers::R6, 0x0U);
-	step();
+	getCpu().regs().set(registers::R1, 0x7FFFFFFFU);
+	Step();
+	ExpectThat().XPSRFlagsEquals("nZc");
+	ExpectThat().Register(registers::R6).Equals(0x0U);
 }
 
-TEST_F(CpuTestHarness, asrImmediate_registers::R1by1ToR7)
-{
+MICROMACHINE_TEST_F(asrImmediate, R1by1ToR7, CpuTestFixture) {
+	getCpu().regs().set(registers::R7, 0x77777777);
+	getCpu().regs().set(registers::R1, 0x11111111);
 	code_gen().emit_ins16("00010iiiiimmmddd", IMM_1, registers::R1, registers::R7);
-	setExpectedXPSRflags("nzC");
-	setExpectedRegisterValue(registers::R7, (int32_t) 0x11111111U >> 1);
-	step();
+	Step();
+	ExpectThat().XPSRFlagsEquals("nzC");
+	ExpectThat().Register(registers::R7).Equals((int32_t)0x11111111U >> 1);
 }
 
-TEST_F(CpuTestHarness, asrImmediate_registers::R7by1ToR2)
-{
+MICROMACHINE_TEST_F(asrImmediate, R7by1ToR2, CpuTestFixture) {
+	getCpu().regs().set(registers::R7, 0x77777777);
+	getCpu().regs().set(registers::R2, 0x22222222);
 	code_gen().emit_ins16("00010iiiiimmmddd", IMM_1, registers::R7, registers::R2);
-	setExpectedXPSRflags("nzC");
-	setExpectedRegisterValue(registers::R2, (int32_t) 0x77777777U >> 1);
-	step();
+	Step();
+	ExpectThat().XPSRFlagsEquals("nzC");
+	ExpectThat().Register(registers::R2).Equals((int32_t)0x77777777U >> 1);
 }
 
-TEST_F(CpuTestHarness, asrImmediate_registers::R0by1)
-{
+MICROMACHINE_TEST_F(asrImmediate, R0by1, CpuTestFixture) {
 	code_gen().emit_ins16("00010iiiiimmmddd", IMM_1, registers::R0, registers::R0);
-	setExpectedXPSRflags("nZc");
-	setExpectedRegisterValue(registers::R0, (int32_t) 0x00000000U >> 1);
-	step();
+	Step();
+	ExpectThat().XPSRFlagsEquals("nZc");
+	ExpectThat().Register(registers::R0).Equals((int32_t)0x00000000U >> 1);
 }
