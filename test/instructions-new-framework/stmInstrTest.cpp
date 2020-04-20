a4810c6edf5e1f05c66a27210571ad0d74788fde
Flavio Roth <flavio.roth@gmail.com>
2020-04-20 18:05:37 +0200

    Migrates stm instruction to new test framework.

--- test/instructions-new-framework/stmInstrTest.cpp
+++ test/instructions-new-framework/stmInstrTest.cpp
@@ -11,77 +11,96 @@
     GNU General Public License for more details.
 */
 
-#include "CpuTestHarness.hpp"
+#include "CpuTestFixture.hpp"
 
 
 /* PUSH
    Encoding: 1100 0 Rn:3 RegisterList:8 */
-TEST_F(CpuTestHarness, stm_JustPushR0WithR7AsAddress)
-{
+MICROMACHINE_TEST_F(stm, JustPushR0WithR7AsAddress, CpuTestFixture) {
+	const uint32_t INITIAL_PC = code_gen().write_address();
+	getCpu().regs().set_pc(INITIAL_PC);
 	code_gen().emit_ins16("11000nnnrrrrrrrr", registers::R7, (1 << 0));
-	setRegisterValue(registers::R7, INITIAL_PC + 16);
-	setExpectedRegisterValue(registers::R7, INITIAL_PC + 16 + 1 * 4);
-	memory_write_32(INITIAL_PC + 16, 0xFFFFFFFF);
-	step();
-	EXPECT_EQ(0x0, memory_read_32(INITIAL_PC + 16));
+	getCpu().regs().set(registers::R7, INITIAL_PC + 16);
+	getCpu().mem().write32(INITIAL_PC + 16, 0xFFFFFFFF);
+	Step();
+	ExpectThat().Register(registers::R7).Equals(INITIAL_PC + 16 + 1 * 4);
+	EXPECT_EQ(0x0, getCpu().mem().read32(INITIAL_PC + 16));
 }
 
-TEST_F(CpuTestHarness, stm_JustPushR7WithR0AsAddress)
-{
+MICROMACHINE_TEST_F(stm, JustPushR7WithR0AsAddress, CpuTestFixture) {
+	const uint32_t INITIAL_PC = code_gen().write_address();
+	getCpu().regs().set_pc(INITIAL_PC);
+	getCpu().regs().set(registers::R7, 0x77777777);
 	code_gen().emit_ins16("11000nnnrrrrrrrr", registers::R0, (1 << 7));
-	setRegisterValue(registers::R0, INITIAL_PC + 16);
-	setExpectedRegisterValue(registers::R0, INITIAL_PC + 16 + 1 * 4);
-	memory_write_32(INITIAL_PC + 16, 0xFFFFFFFF);
-	step();
-	EXPECT_EQ(0x77777777, memory_read_32(INITIAL_PC + 16));
+	getCpu().regs().set(registers::R0, INITIAL_PC + 16);
+	getCpu().mem().write32(INITIAL_PC + 16, 0xFFFFFFFF);
+	Step();
+	ExpectThat().Register(registers::R0).Equals(INITIAL_PC + 16 + 1 * 4);
+	EXPECT_EQ(0x77777777, getCpu().mem().read32(INITIAL_PC + 16));
 }
 
-TEST_F(CpuTestHarness, stm_PushAllWithR0AsAddress)
-{
+MICROMACHINE_TEST_F(stm, PushAllWithR0AsAddress, CpuTestFixture) {
+	const uint32_t INITIAL_PC = code_gen().write_address();
+	getCpu().regs().set_pc(INITIAL_PC);
 	code_gen().emit_ins16("11000nnnrrrrrrrr", registers::R0, 0xFF);
-	setRegisterValue(registers::R0, INITIAL_PC + 16);
-	setExpectedRegisterValue(registers::R0, INITIAL_PC + 16 + 8 * 4);
-	for (int i = 0; i < 8; i++)
-		memory_write_32(INITIAL_PC + 16 + 4 * i, 0xFFFFFFFF);
-	step();
-	EXPECT_EQ(INITIAL_PC + 16, memory_read_32(INITIAL_PC + 16 + 4 * 0));
-	for (int i = 1; i < 8; i++)
-		EXPECT_EQ(0x11111111U * i, memory_read_32(INITIAL_PC + 16 + 4 * i));
+	getCpu().regs().set(registers::R0, INITIAL_PC + 16);
+
+	for (int i = 1; i < 8; i++) {
+		getCpu().regs().set(i, 0x11111111U * i);
+	}
+
+	for (int i = 0; i < 8; i++) {
+		getCpu().mem().write32(INITIAL_PC + 16 + 4 * i, 0xFFFFFFFF);
+	}
+
+	Step();
+	ExpectThat().Register(registers::R0).Equals(INITIAL_PC + 16 + 8 * 4);
+	EXPECT_EQ(INITIAL_PC + 16, getCpu().mem().read32(INITIAL_PC + 16 + 4 * 0));
+	for (int i = 1; i < 8; i++) {
+		EXPECT_EQ(0x11111111U * i, getCpu().mem().read32(INITIAL_PC + 16 + 4 * i));
+	}
 }
 
-TEST_F(CpuTestHarness, stm_PushAllButR7WithR7AsAddress)
-{
+MICROMACHINE_TEST_F(stm, PushAllButR7WithR7AsAddress, CpuTestFixture) {
+	const uint32_t INITIAL_PC = code_gen().write_address();
+	getCpu().regs().set_pc(INITIAL_PC);
 	code_gen().emit_ins16("11000nnnrrrrrrrr", registers::R7, 0x7F);
-	setRegisterValue(registers::R7, INITIAL_PC + 16);
-	setExpectedRegisterValue(registers::R7, INITIAL_PC + 16 + 7 * 4);
-	for (int i = 0; i < 7; i++)
-		memory_write_32(INITIAL_PC + 16 + 4 * i, 0xFFFFFFFF);
-	step();
-	for (int i = 0; i < 7; i++)
-		EXPECT_EQ(0x11111111U * i, memory_read_32(INITIAL_PC + 16 + 4 * i));
+	getCpu().regs().set(registers::R7, INITIAL_PC + 16);
+
+	for (int i = 0; i < 7; i++) {
+		getCpu().regs().set(i, 0x11111111U * i);
+	}
+
+	for (int i = 0; i < 7; i++) {
+		getCpu().mem().write32(INITIAL_PC + 16 + 4 * i, 0xFFFFFFFF);
+	}
+	Step();
+	ExpectThat().Register(registers::R7).Equals(INITIAL_PC + 16 + 7 * 4);
+	for (int i = 0; i < 7; i++) {
+		EXPECT_EQ(0x11111111U * i, getCpu().mem().read32(INITIAL_PC + 16 + 4 * i));
+	}
 }
 
-TEST_F(CpuTestHarness, stm_HardFaultFromInvalidMemoryWrite)
-{
+MICROMACHINE_TEST_F(stm, HardFaultFromInvalidMemoryWrite, CpuTestFixture) {
 	code_gen().emit_ins16("11000nnnrrrrrrrr", registers::R0, 1 << 0);
-	setRegisterValue(registers::R0, 0xFFFFFFFC);
-	setExpectedExceptionTaken(CPU_STEP_HARDFAULT);
-	step();
+	getCpu().regs().set(registers::R0, 0xFFFFFFFC);
+	Step();
+	ExpectThat().HardfaultHandlerReached();
 }
 /*
 TEST_SIM_ONLY(stm, UnpredictableToPushNoRegisters)
 {
     code_gen().emit_ins16("11000nnnrrrrrrrr", registers::R0, 0);
     setExpectedStepReturn(CPU_STEP_UNPREDICTABLE);
-    setExpectedRegisterValue(registers::PC, INITIAL_PC);
+    ExpectThat().Register(registers::PC).Equals(INITIAL_PC);
     pinkySimStep(&m_context);
 }
 
 TEST_SIM_ONLY(stm, UnpredictableToPushWritebackRegisterWhichIsntFirstSaved)
 {
     code_gen().emit_ins16("11000nnnrrrrrrrr", registers::R7, 0xFF);
-    setRegisterValue(registers::R7, INITIAL_PC + 16);
+    getCpu().regs().set(registers::R7, INITIAL_PC + 16);
     setExpectedStepReturn(CPU_STEP_UNPREDICTABLE);
-    setExpectedRegisterValue(registers::PC, INITIAL_PC);
+    ExpectThat().Register(registers::PC).Equals(INITIAL_PC);
     step(&m_context);
 }*/
