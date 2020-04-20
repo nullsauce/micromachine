c038496b8c196447587539da855833b9f1c62c2b
Flavio Roth <flavio.roth@gmail.com>
2020-04-20 18:30:57 +0200

    Migrates tst reg instruction to new testing framework.

--- test/instructions-new-framework/tstRegisterInstrTest.cpp
+++ test/instructions-new-framework/tstRegisterInstrTest.cpp
@@ -11,41 +11,41 @@
     GNU General Public License for more details.
 */
 
-#include "CpuTestHarness.hpp"
+#include "CpuTestFixture.hpp"
 
 
 /* TST - Register
    Encoding: 010000 1000 Rm:3 Rn:3 */
 /* NOTE: APSR_C state is maintained by this instruction. */
-TEST_F(CpuTestHarness, tstRegister_UseLowestRegisterForBothArgsAndResultShouldBeZero)
-{
+MICROMACHINE_TEST_F(tstRegister, UseLowestRegisterForBothArgsAndResultShouldBeZero, CpuTestFixture) {
 	code_gen().emit_ins16("0100001000mmmnnn", registers::R0, registers::R0);
 	// Use a couple of tests to explicitly set/clear carry to verify both states are maintained.
-	setExpectedXPSRflags("nZc");
-	clearCarry();
-	step();
+	getCpu().regs().app_status_register().write_carry_flag(false);
+	Step();
+	ExpectThat().XPSRFlagsEquals("nZc");
 }
 
-TEST_F(CpuTestHarness, tstRegister_UseHighestRegisterForBothArgsAndRegisterWillBeUnchanged)
-{
+MICROMACHINE_TEST_F(tstRegister, UseHighestRegisterForBothArgsAndRegisterWillBeUnchanged, CpuTestFixture) {
+	getCpu().regs().set(registers::R7, 0x77777777U);
 	code_gen().emit_ins16("0100001000mmmnnn", registers::R7, registers::R7);
-	setExpectedXPSRflags("nzC");
-	setCarry();
-	step();
+	getCpu().regs().app_status_register().write_carry_flag(true);
+	Step();
+	ExpectThat().XPSRFlagsEquals("nzC");
 }
 
-TEST_F(CpuTestHarness, tstRegister_AndR3andR7)
-{
+MICROMACHINE_TEST_F(tstRegister, AndR3andR7, CpuTestFixture) {
+	getCpu().regs().set(registers::R3, 0x33333333U);
+	getCpu().regs().set(registers::R7, 0x77777777U);
 	code_gen().emit_ins16("0100001000mmmnnn", registers::R3, registers::R7);
-	setExpectedXPSRflags("nz");
-	step();
+	Step();
+	ExpectThat().XPSRFlagsEquals("nz");
 }
 
-TEST_F(CpuTestHarness, tstRegister_UseAndToJustKeepNegativeSignBit)
-{
+MICROMACHINE_TEST_F(tstRegister, UseAndToJustKeepNegativeSignBit, CpuTestFixture) {
+	getCpu().regs().set(registers::R7, 0x77777777U);
 	code_gen().emit_ins16("0100001000mmmnnn", registers::R7, registers::R0);
-	setRegisterValue(registers::R0, -1);
-	setRegisterValue(registers::R7, 0x80000000);
-	setExpectedXPSRflags("Nz");
-	step();
+	getCpu().regs().set(registers::R0, -1);
+	getCpu().regs().set(registers::R7, 0x80000000);
+	Step();
+	ExpectThat().XPSRFlagsEquals("Nz");
 }
