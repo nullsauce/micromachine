eda79343faeffee8541c8ac4002aa2a4c0588028
Flavio Roth <flavio.roth@gmail.com>
2020-04-20 18:06:58 +0200

    Migrates strh imm instruction to new test framework.

--- test/instructions-new-framework/strhImmediateInstrTest.cpp
+++ test/instructions-new-framework/strhImmediateInstrTest.cpp
@@ -11,42 +11,49 @@
     GNU General Public License for more details.
 */
 
-#include "CpuTestHarness.hpp"
+#include "CpuTestFixture.hpp"
 
 
 /* STRH - Immediate
    Encoding: 1000 0 Imm:5 Rn:3 Rt:3 */
-TEST_F(CpuTestHarness, strhImmediate_UseAMixOfRegistersWordAlignedWithSmallestOffset)
-{
+MICROMACHINE_TEST_F(strhImmediate, UseAMixOfRegistersWordAlignedWithSmallestOffset, CpuTestFixture) {
+	const uint32_t INITIAL_PC = code_gen().write_address();
+	getCpu().regs().set_pc(INITIAL_PC);
 	code_gen().emit_ins16("10000iiiiinnnttt", 0, registers::R7, registers::R0);
-	setRegisterValue(registers::R7, INITIAL_PC + 4);
-	memory_write_32(INITIAL_PC + 4, 0xBAADFEED);
-	step();
-	EXPECT_EQ(0xBAAD0000, memory_read_32(INITIAL_PC + 4));
+	getCpu().regs().set(registers::R7, INITIAL_PC + 4);
+	getCpu().mem().write32(INITIAL_PC + 4, 0xBAADFEED);
+	Step();
+	EXPECT_EQ(0xBAAD0000, getCpu().mem().read32(INITIAL_PC + 4));
 }
 
-TEST_F(CpuTestHarness, strhImmediate_AnotherMixOfRegistersNotWordAligned)
-{
+MICROMACHINE_TEST_F(strhImmediate, AnotherMixOfRegistersNotWordAligned, CpuTestFixture) {
+	const uint32_t INITIAL_PC = code_gen().write_address();
+	getCpu().regs().set_pc(INITIAL_PC);
+	getCpu().regs().set(registers::R7, 0x77777777U);
 	code_gen().emit_ins16("10000iiiiinnnttt", 1, registers::R0, registers::R7);
-	setRegisterValue(registers::R0, INITIAL_PC + 4);
-	memory_write_32(INITIAL_PC + 4, 0xBAADFEED);
-	step();
-	EXPECT_EQ(0x7777FEED, memory_read_32(INITIAL_PC + 4));
+	getCpu().regs().set(registers::R0, INITIAL_PC + 4);
+	getCpu().mem().write32(INITIAL_PC + 4, 0xBAADFEED);
+	Step();
+	EXPECT_EQ(0x7777FEED, getCpu().mem().read32(INITIAL_PC + 4));
 }
 
-TEST_F(CpuTestHarness, strhImmediate_LargestOffset)
-{
+MICROMACHINE_TEST_F(strhImmediate, LargestOffset, CpuTestFixture) {
+	const uint32_t INITIAL_PC = code_gen().write_address();
+	getCpu().regs().set_pc(INITIAL_PC);
+	getCpu().regs().set(registers::R6, 0x66666666U);
 	code_gen().emit_ins16("10000iiiiinnnttt", 31, registers::R1, registers::R6);
-	setRegisterValue(registers::R1, INITIAL_PC);
-	memory_write_32(INITIAL_PC + 60, 0xBAADFEED);
-	step();
-	EXPECT_EQ(0x6666FEED, memory_read_32(INITIAL_PC + 60));
+	getCpu().regs().set(registers::R1, INITIAL_PC);
+	getCpu().mem().write32(INITIAL_PC + 60, 0xBAADFEED);
+	Step();
+	EXPECT_EQ(0x6666FEED, getCpu().mem().read32(INITIAL_PC + 60));
 }
 
-TEST_F(CpuTestHarness, strhImmediate_AttemptStoreToInvalidAddress)
-{
+MICROMACHINE_TEST_F(strhImmediate, AttemptStoreToInvalidAddress, CpuTestFixture) {
+	const uint32_t INITIAL_PC = code_gen().write_address();
+	getCpu().regs().set_pc(INITIAL_PC);
+	getCpu().regs().set(registers::R1, 0x11111111U);
 	code_gen().emit_ins16("10000iiiiinnnttt", 0, registers::R3, registers::R1);
-	setRegisterValue(registers::R3, 0xFFFFFFFC);
-	setExpectedExceptionTaken(CPU_STEP_HARDFAULT);
-	step();
+	getCpu().regs().set(registers::R3, 0xFFFFFFFC);
+	Step();
+	ExpectThat().HardfaultHandlerReached();
 }
