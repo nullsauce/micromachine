f2b470eb1561884b1894ac91e7ed4238c38df21e
Flavio Roth <flavio.roth@gmail.com>
2020-04-20 18:04:15 +0200

    Migrates strh reg instruction to new test framework.

--- test/instructions-new-framework/strhRegisterInstrTest.cpp
+++ test/instructions-new-framework/strhRegisterInstrTest.cpp
@@ -11,56 +11,63 @@
     GNU General Public License for more details.
 */
 
-#include "CpuTestHarness.hpp"
+#include "CpuTestFixture.hpp"
 
 
 /* STRH - Register
    Encoding: 0101 001 Rm:3 Rn:3 Rt:3 */
-TEST_F(CpuTestHarness, strhRegister_UseAMixOfRegistersWordAligned)
-{
+MICROMACHINE_TEST_F(strhRegister, UseAMixOfRegistersWordAligned, CpuTestFixture) {
+	const uint32_t INITIAL_PC = code_gen().write_address();
+	getCpu().regs().set_pc(INITIAL_PC);
 	code_gen().emit_ins16("0101001mmmnnnttt", registers::R7, registers::R3, registers::R0);
-	setRegisterValue(registers::R3, INITIAL_PC);
-	setRegisterValue(registers::R7, 4);
-	memory_write_32(INITIAL_PC + 4, 0xBAADFEED);
-	step();
-	EXPECT_EQ(0xBAAD0000, memory_read_32(INITIAL_PC + 4));
+	getCpu().regs().set(registers::R3, INITIAL_PC);
+	getCpu().regs().set(registers::R7, 4);
+	getCpu().mem().write32(INITIAL_PC + 4, 0xBAADFEED);
+	Step();
+	EXPECT_EQ(0xBAAD0000, getCpu().mem().read32(INITIAL_PC + 4));
 }
 
-TEST_F(CpuTestHarness, strhRegister_UseAnotherMixOfRegistersWordAligned)
-{
+MICROMACHINE_TEST_F(strhRegister, UseAnotherMixOfRegistersWordAligned, CpuTestFixture) {
+	const uint32_t INITIAL_PC = code_gen().write_address();
+	getCpu().regs().set_pc(INITIAL_PC);
+	getCpu().regs().set(registers::R7, 0x77777777);
 	code_gen().emit_ins16("0101001mmmnnnttt", registers::R1, registers::R0, registers::R7);
-	setRegisterValue(registers::R0, INITIAL_PC);
-	setRegisterValue(registers::R1, 4);
-	memory_write_32(INITIAL_PC + 4, 0xBAADFEED);
-	step();
-	EXPECT_EQ(0xBAAD7777, memory_read_32(INITIAL_PC + 4));
+	getCpu().regs().set(registers::R0, INITIAL_PC);
+	getCpu().regs().set(registers::R1, 4);
+	getCpu().mem().write32(INITIAL_PC + 4, 0xBAADFEED);
+	Step();
+	EXPECT_EQ(0xBAAD7777, getCpu().mem().read32(INITIAL_PC + 4));
 }
 
-TEST_F(CpuTestHarness, strhRegister_YetAnotherMixOfRegistersNotWordAligned)
-{
+MICROMACHINE_TEST_F(strhRegister, YetAnotherMixOfRegistersNotWordAligned, CpuTestFixture) {
+	const uint32_t INITIAL_PC = code_gen().write_address();
+	getCpu().regs().set_pc(INITIAL_PC);
+	getCpu().regs().set(registers::R4, 0x44444444);
 	code_gen().emit_ins16("0101001mmmnnnttt", registers::R0, registers::R7, registers::R4);
-	setRegisterValue(registers::R7, INITIAL_PC);
-	setRegisterValue(registers::R0, 6);
-	memory_write_32(INITIAL_PC + 4, 0xBAADFEED);
-	step();
-	EXPECT_EQ(0x4444FEED, memory_read_32(INITIAL_PC + 4));
+	getCpu().regs().set(registers::R7, INITIAL_PC);
+	getCpu().regs().set(registers::R0, 6);
+	getCpu().mem().write32(INITIAL_PC + 4, 0xBAADFEED);
+	Step();
+	EXPECT_EQ(0x4444FEED, getCpu().mem().read32(INITIAL_PC + 4));
 }
 
-TEST_F(CpuTestHarness, strhRegister_AttemptUnalignedStore)
-{
+MICROMACHINE_TEST_F(strhRegister, AttemptUnalignedStore, CpuTestFixture) {
+	const uint32_t INITIAL_PC = code_gen().write_address();
+	getCpu().regs().set_pc(INITIAL_PC);
 	code_gen().emit_ins16("0101001mmmnnnttt", registers::R7, registers::R3, registers::R0);
-	setRegisterValue(registers::R3, INITIAL_PC + 1024);
-	setRegisterValue(registers::R7, 1);
-	setExpectedExceptionTaken(CPU_STEP_HARDFAULT);
-	memory_write_32(INITIAL_PC + 1024, 0xBAADFEED);
-	step();
+	getCpu().regs().set(registers::R3, INITIAL_PC + 1024);
+	getCpu().regs().set(registers::R7, 1);
+	getCpu().mem().write32(INITIAL_PC + 1024, 0xBAADFEED);
+	Step();
+	ExpectThat().HardfaultHandlerReached();
 }
 
-TEST_F(CpuTestHarness, strhRegister_AttemptStoreToInvalidAddress)
-{
+MICROMACHINE_TEST_F(strhRegister, AttemptStoreToInvalidAddress, CpuTestFixture) {
+	const uint32_t INITIAL_PC = code_gen().write_address();
+	getCpu().regs().set_pc(INITIAL_PC);
 	code_gen().emit_ins16("0101001mmmnnnttt", registers::R7, registers::R3, registers::R0);
-	setRegisterValue(registers::R3, 0xFFFFFFFC);
-	setRegisterValue(registers::R7, 0);
-	setExpectedExceptionTaken(CPU_STEP_HARDFAULT);
-	step();
+	getCpu().regs().set(registers::R3, 0xFFFFFFFC);
+	getCpu().regs().set(registers::R7, 0);
+	Step();
+	ExpectThat().HardfaultHandlerReached();
 }
