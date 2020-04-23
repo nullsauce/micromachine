419333f918d4a971dc4394b0763a8bcd6feca0fc
Flavio Roth <flavio.roth@gmail.com>
2020-04-23 15:14:38 +0200

    Cleanup

--- micromachine/helpers/check.hpp
+++ micromachine/helpers/check.hpp
@@ -7,21 +7,27 @@
 #include <cstdio>
 #include <exception>
 
-#define micromachine_fail(message, ...)                                                                 \
-	{                                                                                              \
-		fprintf(stderr, "where: %s line %d\n", __FILE__, __LINE__);                                \
-		fprintf(stderr, message, ##__VA_ARGS__);                                                   \
-		std::terminate();                                                                          \
-	}
+#define __DEBUG_STR2__(x) #x
+#define __DEBUG_STR__(x) __DEBUG_STR2__(x)
+__glibcxx_assert()
+#define micromachine_fail(message, ...)                                                        \
+{                                                                                              \
+	fprintf(stderr, "where: %s line %d\n", __FILE__, __LINE__);                                \
+	fprintf(stderr, "micromachine fatal error: " message "\n", ##__VA_ARGS__);                 \
+	std::terminate();                                                                          \
+}
+
 
 #ifdef MICROMACHINE_ENABLE_CHECKS
-#define micromachine_check(cond, message, ...)                                                                \
-	{                                                                                              \
-		if(!(cond)) {                                                                              \
-			micromachine_fail("FATAL: " message "\n", ##__VA_ARGS__)                                    \
-		}                                                                                          \
-	}
-
+#define micromachine_check(cond, message, ...)                                                 \
+{                                                                                              \
+	if(!(cond)) {                                                                              \
+		micromachine_fail("the following check failed: " #cond "\nreason: " message, ##__VA_ARGS__)      \
+	}                                                                                          \
+}
 #else
 #define micromachine_check(...) (void)0
 #endif
+
+#undef __DEBUG_STR2__
+#undef __DEBUG_STR__
