5bb9bd4f14fe6ff08b336d88b49c2a41717f9213
Flavio Roth <flavio.roth@gmail.com>
2020-04-30 22:50:08 +0200

    Adds a test suite for the usart peripheral

--- micromachine-test/framework/CpuTestFixture.hpp
+++ micromachine-test/framework/CpuTestFixture.hpp
@@ -13,14 +13,6 @@ and/or distributed without the express permission of Flavio Roth.
 
 #include "MachineTestHarness.hpp"
 
-/**
- * Utility macro to declare tests with a test_case_name different
- * from the fixture class name.
- */
-#define MICROMACHINE_TEST_F(test_case_name, test_name, test_fixture)\
-  GTEST_TEST_(test_case_name, test_name, test_fixture, \
-              ::testing::internal::GetTypeId<test_fixture>())
-
 
 namespace micromachine::testing {
 
