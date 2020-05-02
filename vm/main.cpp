e73e303097237a650fcfc2c6560d2e0d15c1644a
Flavio Roth <flavio.roth@gmail.com>
2020-05-02 09:48:46 +0200

    Adds a vm option to spin up the experimental debugger

--- vm/main.cpp
+++ vm/main.cpp
@@ -5,6 +5,7 @@
 #include "mcu.hpp"
 #include "peripherals/iodev.hpp"
 #include "programmer.hpp"
+#include "gdb-server.hpp"
 
 #include <chrono>
 #include <unistd.h>
@@ -15,12 +16,16 @@ int main(int argc, char** argv) {
 	bool testing_enabled = false;
 	std::string executable_path;
 	std::string usart_input_string;
+	bool enable_gdb_server = false;
+	uint16_t gdb_server_port = 2345;
 
 	cxxopts::Options options("micromachine", "ARMv6-M emulator");
 	options.add_options()
 		("t,testing", "Enable testing", cxxopts::value<bool>()->default_value("false"))
 		("executable", "Executable path", cxxopts::value<std::string>())
 		("usart-input-string", "usart input data to be passed to the application", cxxopts::value<std::string>())
+		("gdb-server", "Enable gdb server debugging", cxxopts::value<bool>()->default_value("true"))
+		("gdb-server-port", "sets the gdb server port (implies gdb-server)", cxxopts::value<uint16_t>()->default_value("2345"))
 		("h,help", "Print usage")
 	;
 
@@ -44,6 +49,8 @@ int main(int argc, char** argv) {
 
 		testing_enabled = result["testing"].as<bool>();
 		executable_path = result["executable"].as<std::string>();
+		enable_gdb_server = result["gdb-server"].as<bool>();
+		gdb_server_port = result["gdb-server-port"].as<uint16_t>();
 
 	} catch (const cxxopts::OptionParseException& e) {
 		fprintf(stderr, "%s\n", e.what());
@@ -77,25 +84,30 @@ int main(int argc, char** argv) {
 
 	mcu.reset(program->entry_point());
 
-	auto start = std::chrono::steady_clock::now();
-	decltype(start) end;
-	for(;;) {
-		micromachine::system::cpu::step_result state = mcu.step();
-		if(state == micromachine::system::cpu::step_result::BREAK || state == micromachine::system::cpu::step_result::FAULT) {
-			end = std::chrono::steady_clock::now();
-			break;
+	if(enable_gdb_server) {
+		fprintf(stderr, "Spawning gdb server on port %u\n", gdb_server_port);
+		gdb_server(mcu.get_cpu(), program);
+	} else {
+		auto start = std::chrono::steady_clock::now();
+		decltype(start) end;
+		for(;;) {
+			micromachine::system::cpu::step_result state = mcu.step();
+			if(state == micromachine::system::cpu::step_result::BREAK ||
+			   state == micromachine::system::cpu::step_result::FAULT) {
+				end = std::chrono::steady_clock::now();
+				break;
+			}
 		}
-	}
-	uint64_t elapsed_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
-	double elapsed_secs = elapsed_ms/1000.0;
-	fprintf(stderr, "elapsed: %f seconds\n", elapsed_secs);
+		uint64_t elapsed_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+		double elapsed_secs = elapsed_ms / 1000.0;
+		fprintf(stderr, "elapsed: %f seconds\n", elapsed_secs);
 
-	uint64_t instructions_executed = mcu.get_cpu().debug_instruction_counter();
-	double perf = instructions_executed / elapsed_secs;
-	fprintf(stderr, "run %ld instruction(s), %f i/s\n", instructions_executed, perf);
+		uint64_t instructions_executed = mcu.get_cpu().debug_instruction_counter();
+		double perf = instructions_executed / elapsed_secs;
+		fprintf(stderr, "run %ld instruction(s), %f i/s\n", instructions_executed, perf);
+	}
 
 	iopump.shutdown();
 
-	//c.regs().print();
 	return EXIT_SUCCESS;
 }
