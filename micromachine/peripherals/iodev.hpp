38cdf09e2d6d148ea53ee6e1da0a8e7f5b69d0f4
Flavio Roth <flavio.roth@gmail.com>
2020-05-02 13:53:01 +0200

    The vm now waits for the usart io to be completely flushed before exiting. This was causing the sdk test to randomly fail.

--- micromachine/peripherals/iodev.hpp
+++ micromachine/peripherals/iodev.hpp
@@ -12,6 +12,11 @@ class iodev {
 public:
 	virtual void send(uint8_t byte) = 0;
 	virtual bool receive(uint8_t& byte) = 0;
+	/**
+	 * The device has at least that many bytes available for receiving.
+	 * @return
+	 */
+	virtual size_t  bytes_available() const = 0;
 	virtual void shutdown() = 0;
 	void write_string(const std::string& string) {
 		for(char c : string) {
@@ -37,6 +42,26 @@ public:
 		, _aborted(false)
 		, _thread(&iopump::loop, this)
 	{}
+
+	/**
+	 * Waits at most timeout_millis milliseconds for all the underlying device's data to be passed to the callback.
+	 * @param timeout_millis The maximmum amount of time this operation is given. Pass zero for infinite.
+	 * @return true if all the data could be flushed before the timeout
+	 */
+	bool wait_until_flushed(uint64_t timeout_millis = 0) {
+		auto start = std::chrono::steady_clock::now();
+		while(_iodev.bytes_available() > 0) {
+			if(timeout_millis) {
+				auto elapsed = std::chrono::steady_clock::now() - start;
+				uint64_t elapsed_millis = std::chrono::duration_cast<std::chrono::milliseconds>(elapsed).count();
+				if(elapsed_millis > timeout_millis) {
+					return _iodev.bytes_available() == 0;
+				}
+			}
+			std::this_thread::sleep_for(std::chrono::milliseconds(1));
+		}
+		return true;
+	}
 
 	void shutdown() {
 		_aborted = true;
