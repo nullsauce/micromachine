b27fcf84f2609b1fa480255f6240a5b3f2b8136e
Flavio Roth <flavio.roth@gmail.com>
2020-05-06 12:58:46 +0200

    Fixes memory value predicates printing incorrect message when failing.

--- micromachine-test/framework/predicates/MemoryValue32Predicate.hpp
+++ micromachine-test/framework/predicates/MemoryValue32Predicate.hpp
@@ -14,31 +14,32 @@ namespace micromachine::testing {
 
 class MemoryValue32Predicate : public MemoryValuePredicate {
 protected:
-	const uint32_t _value;
+	const uint32_t _expectedValue;
 
 public:
 	MemoryValue32Predicate(uint32_t address, uint32_t value)
 		: MemoryValuePredicate(address)
-		, _value(value)
+		, _expectedValue(value)
 	{}
 
 	void apply(mcu& expected) {
-		if(!expected.get_memory().write32(_address, _value)) {
-			micromachine_fail("MemoryValue32Predicate applied at an unmapped address. address=%08x value=%08x", _address, _value);
+		if(!expected.get_memory().write32(_address, _expectedValue)) {
+			micromachine_fail("MemoryValue32Predicate applied at an unmapped address. address=%08x value=%08x", _address,
+							  _expectedValue);
 		}
 	}
 
 	void check(const mcu& actual) const {
 		uint32_t actualValue = actual.get_memory().read32_unchecked(_address);
-		EXPECT_PRED_FORMAT3(assertMemoryEquality, actualValue, _value, _address);
+		EXPECT_PRED_FORMAT3(assertMemoryEquality, actualValue, _expectedValue, _address);
 	}
 
 private:
 	static ::testing::AssertionResult assertMemoryEquality(const char*,
 														   const char*,
 														   const char*,
-														   uint32_t expectedValue,
 														   uint32_t actualValue,
+														   uint32_t expectedValue,
 														   uint32_t address) {
 
 		if(expectedValue == actualValue) {
