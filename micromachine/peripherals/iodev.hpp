4bd85f965db05c9eea06e954feebf9612ba721a7
jjungo <j.jungo@gmail.com>
2020-05-13 09:43:56 +0200

    move echoer_iodevice and empy_iodevice in framework/IodeviceHelpers.hpp

--- micromachine/peripherals/iodev.hpp
+++ micromachine/peripherals/iodev.hpp
@@ -28,42 +28,6 @@ public:
 	}
 };
 
-/**
- * An echoer iodevice that act as a loop back.
- *
- * There is no rx buffer. send push into tx buffer, and receive pop it.
- *
- * @tparam buffer_type
- * @tparam buffer_size
- */
-template <typename buffer_type, size_t buffer_size>
-class echoer_iodevice : public iodev {
-private:
-	using data_channel = blocking_bounded_channel<buffer_type, buffer_size>;
-	data_channel _tx_buffer;
-
-public:
-	/**
-	 * Send byte to the tx buffer and create a loopback for echoing messages
-	 * @param byte
-	 */
-	void send(uint8_t byte) override {
-		_tx_buffer.force_push(byte);
-	}
-
-	bool receive(uint8_t& out) override {
-		return data_channel::success == _tx_buffer.pop(out);
-	}
-
-	void shutdown() override {
-		_tx_buffer.abort();
-	}
-
-	size_t bytes_available() const override {
-		return _tx_buffer.size();
-	}
-};
-
 class iopump {
 public:
 	using rx_callback = std::function<void(uint8_t byte)>;
