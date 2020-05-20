d5f8d2d71cab1ac15f29a08d767511b32be23110
Flavio Roth <flavio.roth@gmail.com>
2020-05-20 11:44:07 +0200

    Adds a waitable counter

---
+++ micromachine/utils/waitable_counter.hpp
@@ -0,0 +1,35 @@
+
+//
+// Created by fla on 04.05.20.
+// Copyright (c) 2020 The Micromachine Project. All rights reserved.
+//
+
+#pragma once
+
+#include "waitable_value.hpp"
+
+namespace micromachine::system {
+
+/**
+ * A waitable counter is an unsigned waitable counter on which
+ * threads can wait for a specific value.
+ */
+class waitable_counter : public waitable_value<size_t> {
+public:
+	using waitable_value::waitable_value;
+
+	void decrement() {
+		std::unique_lock<std::mutex> lock(_mutex);
+		_value--;
+		_signal.notify_all();
+	}
+
+	void increment() {
+		std::unique_lock<std::mutex> lock(_mutex);
+		_value++;
+		_signal.notify_all();
+	}
+
+};
+
+}
